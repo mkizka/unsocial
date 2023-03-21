92b0200ca3b08e8e7770435f88f8e58e9c49eada
mkizka <g.mkizka@gmail.com>
2023-03-21 16:47:51 +0900

    エラー表示改善

--- scripts/setup-for-docker.sh
+++ scripts/setup-for-docker.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/bash
 set -eu
 
+function has() {
+  type "$1" > /dev/null 2>&1
+}
+
+if ! has mkcert; then
+  echo "ERROR: You have to install mkcert (https://github.com/FiloSottile/mkcert#installation)"
+  exit 1;
+fi
+
 echo "Install local CA and copy to ./docker/mkcert"
 mkcert -install 2>/dev/null
 mkdir -p docker/mkcert 2>/dev/null
@@ -13,17 +22,21 @@ mkcert -cert-file docker/nginx/certs/misskey.crt -key-file docker/nginx/certs/mi
 mkcert -cert-file docker/nginx/certs/soshal.crt -key-file docker/nginx/certs/soshal.key soshal.localhost 2>/dev/null
 echo "Done"
 
-has() {
-  type "$1" > /dev/null 2>&1
-}
-
 if has wslpath; then
+  echo "WSL Detected"
+  function pwsh() {
+    /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -c "$@"
+  }
+  if ! pwsh mkcert -help > dev/null 2>&1; then
+    echo "ERROR: You have to install mkcert on Windows (https://github.com/FiloSottile/mkcert#windows)"
+    exit 1;
+  fi
+
   echo "Install local CA to Windows"
-  PWSH="/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
-  WIN_CAROOT="$(wslpath "$("$PWSH" -c mkcert -CAROOT)")"
+  WIN_CAROOT="$(wslpath "$(pwsh mkcert -CAROOT)")"
   mkdir -p "$WIN_CAROOT"
   cp -f "$(mkcert -CAROOT)"/rootCA.pem "$WIN_CAROOT"/
-  "$PWSH" -c mkcert -install 2>/dev/null
+  pwsh mkcert -install 2>/dev/null
   echo "Done"
 fi
 
