b0eba53adf32ff532463a15f6ffe932e7cf84e40
mkizka <g.mkizka@gmail.com>
2023-03-22 23:36:21 +0900

    show diff result

--- scripts/mutation-test-local.sh
+++ scripts/mutation-test-local.sh
@@ -3,5 +3,10 @@ set -eu
 
 rm -rf reports
 mkdir -p reports
-curl -o reports/stryker-incremental.json https://minio-s3.paas.mkizka.dev/soshal-mutation-test/main/stryker-incremental.json
+curl -s -o reports/stryker-incremental.json https://minio-s3.paas.mkizka.dev/soshal-mutation-test/main/stryker-incremental.json
 pnpm stryker run
+
+rm -rf reports-main
+mkdir -p reports-main/mutation
+curl -s -o reports-main/mutation/mutation.json https://minio-s3.paas.mkizka.dev/soshal-mutation-test/main/mutation/mutation.json
+./ci/report-mutation-test-pr.mts | npx cli-markdown
