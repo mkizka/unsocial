06249bac169b06f16a57d25149ba329d22f0a157
mkizka <g.mkizka@gmail.com>
2023-03-26 00:29:11 +0900

    add husky lint-staged

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,11 @@
+import path from "path";
+
+// https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+export default {
+  "*.{js,mjs,ts,mts,tsx}": [buildEslintCommand],
+};
