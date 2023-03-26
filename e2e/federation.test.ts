90a2e76d76effa9e0c23b7e109e2e708e2322b62
mkizka <g.mkizka@gmail.com>
2023-03-26 23:55:04 +0900

    update

--- e2e/federation.test.ts
+++ e2e/federation.test.ts
@@ -1,8 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("他サーバーのアカウントを表示できる", async ({ page }) => {
-  const response = await page.goto(
-    "https://soshal.localhost/@e2e@misskey.localhost"
-  );
+  const response = await page.goto("/@e2e@misskey.localhost");
   expect(response?.status()).toBe(200);
 });
