2413cfa8dc4d63a1a0fa02429b502b2cfd75c059
mkizka <g.mkizka@gmail.com>
2023-03-26 23:03:24 +0900

    update

--- e2e/federation.test.ts
+++ e2e/federation.test.ts
@@ -1,16 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("他サーバーのアカウントを表示できる", async ({ page }) => {
-  // Misskeyにアカウントを作成する
-  await page.goto("https://misskey.localhost");
-  await page.click("[data-cy-signup]");
-  await page.locator("[data-cy-signup-username] input").fill("e2e");
-  await page.locator("[data-cy-signup-password] input").fill("e2e");
-  await page.locator("[data-cy-signup-password-retype] input").fill("e2e");
-  await page.locator(".tou").click();
-  await page.locator("[data-cy-signup-submit]").click();
-
-  // SoshalにアクセスしてMisskeyのアカウントを表示する
   const response = await page.goto(
     "https://soshal.localhost/@e2e@misskey.localhost"
   );
