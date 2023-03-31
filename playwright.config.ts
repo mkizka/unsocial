5a8157df8bd7be8acc23d0284600e94739cc8287
mkizka <g.mkizka@gmail.com>
2023-04-01 00:47:00 +0900

    fix baseURL

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "e2e",
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "https://soshal.localhost",
     video: "retain-on-failure",
     ignoreHTTPSErrors: true,
   },
