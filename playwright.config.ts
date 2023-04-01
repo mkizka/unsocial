a4b95c37cb33868b70d65dd169adcd8d2bc44e26
mkizka <g.mkizka@gmail.com>
2023-04-01 22:45:14 +0900

    retries: 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "e2e",
+  retries: 3,
   use: {
     baseURL: "https://soshal.localhost",
     video: "retain-on-failure",
