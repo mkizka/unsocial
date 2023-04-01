5450f57f30c564c16c8e346229fe3e4d62ac028b
mkizka <g.mkizka@gmail.com>
2023-04-02 00:07:42 +0900

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "e2e",
-  retries: 3,
+  retries: 5,
   use: {
     baseURL: "https://soshal.localhost",
     video: "retain-on-failure",
