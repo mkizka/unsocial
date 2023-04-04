23bda4b96fdc8ee2119bd62dea4ad4c08412ac51
mkizka <g.mkizka@gmail.com>
2023-04-05 01:14:24 +0900

    フォロー/アンフォローのe2eテスト追加

--- e2e/federation.test.ts
+++ e2e/federation.test.ts
@@ -12,29 +12,50 @@ const postNote = async (page: Page, content: string) => {
 
 test.describe.configure({ mode: "parallel" });
 
+test.use({ storageState: "e2e/state.json" });
+
 test.describe("Federation", () => {
-  test.use({ storageState: "e2e/state.json" });
+  test.describe("Note", () => {
+    test("作成 → 削除が同期される", async ({ page }) => {
+      // 投稿
+      await page.goto("/");
+      const content = `投稿テスト${crypto.randomUUID()}`;
+      const note = await postNote(page, content);
 
-  test("作成したノートが他サーバーに連合される", async ({ page }) => {
-    page.goto("/");
-    const content = `投稿テスト${crypto.randomUUID()}`;
-    await postNote(page, content);
+      // Misskey側で確認
+      await page.goto("https://misskey.localhost");
+      await page.locator("button", { hasText: "Global" }).click();
+      await expect(page.locator(`text=${content}`)).toBeVisible();
 
-    page.goto("https://misskey.localhost");
-    await page.locator("button", { hasText: "Global" }).click();
-    await expect(page.locator(`text=${content}`)).toBeVisible();
+      // 削除
+      await page.goto("/");
+      await note.getByTestId("delete-button").click();
+
+      // Misskey側で確認
+      await page.goto("https://misskey.localhost");
+      await page.locator("button", { hasText: "Global" }).click();
+      await expect(page.locator(`text=${content}`)).not.toBeVisible();
+    });
   });
 
-  test("削除したノートが他サーバーから削除される", async ({ page }) => {
-    page.goto("/");
-    const content = `削除テスト${crypto.randomUUID()}`;
-    const note = await postNote(page, content);
-    // Misskey側の保存処理を待つ
-    await page.waitForTimeout(1000);
-    await note.getByTestId("delete-button").click();
+  test.describe("Follow", () => {
+    test("フォロー → フォロー解除が同期される", async ({ page }) => {
+      // フォロー
+      await page.goto("/@e2e@misskey.localhost");
+      await page.getByTestId("follow-button").click();
 
-    page.goto("https://misskey.localhost");
-    await page.locator("button", { hasText: "Global" }).click();
-    await expect(page.locator(`text=${content}`)).not.toBeVisible();
+      // Misskey側で確認
+      await page.goto("https://misskey.localhost/@e2e/followers");
+      const testAccountInMisskey = page.locator("text=@test@soshal.localhost");
+      await expect(testAccountInMisskey).toBeVisible();
+
+      // フォロー解除
+      await page.goto("/@e2e@misskey.localhost");
+      await page.getByTestId("follow-button").click();
+
+      // Misskey側で確認
+      await page.goto("https://misskey.localhost/@e2e/followers");
+      await expect(testAccountInMisskey).not.toBeVisible();
+    });
   });
 });
