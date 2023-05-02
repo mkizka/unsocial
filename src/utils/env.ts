5c1cdec97073f5a6926229740d708ce2b839e85c
mkizka <g.mkizka@gmail.com>
2023-05-02 23:11:01 +0900

    env修正

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,9 +3,14 @@ import { z } from "zod";
 
 import { formatZodError } from "./formatZodError";
 
+// Vercelでの実行時にPOSTGRES_PRISMA_URLは存在しない
+const dbUrlSchema = process.env.VERCEL
+  ? z.string().url().optional()
+  : z.string().url();
+
 const serverEnvSchema = z.object({
-  POSTGRES_PRISMA_URL: z.string().url(),
-  POSTGRES_URL_NON_POOLING: z.string().url(),
+  POSTGRES_PRISMA_URL: dbUrlSchema,
+  POSTGRES_URL_NON_POOLING: dbUrlSchema,
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
@@ -25,7 +30,7 @@ const serverEnvSchema = z.object({
   EMAIL_SERVER_PASS: z.string().default("password"),
   EMAIL_SERVER_HOST: z.string(),
   EMAIL_SERVER_PORT: z.coerce.number(),
-  EMAIL_FROM: z.string().email(),
+  EMAIL_FROM: z.string().email().default("from@example.com"),
 });
 
 let env = process.env as unknown as z.infer<typeof serverEnvSchema>;
