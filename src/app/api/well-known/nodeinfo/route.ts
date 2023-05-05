219626391ddd424af633446b60fece052fb926ac
mkizka <g.mkizka@gmail.com>
2023-05-05 23:48:01 +0900

    確認

--- src/app/api/well-known/nodeinfo/route.ts
+++ src/app/api/well-known/nodeinfo/route.ts
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server";
 import { env } from "@/utils/env";
 
 export async function GET() {
-  console.log(process.env.VERCEL_URL);
   return NextResponse.json(
     {
       links: [
         {
           rel: "http://nodeinfo.diaspora.software/ns/schema/2.1",
           href: `https://${env.HOST}/.well-known/nodeinfo/2.1`,
+          VERCEL_URL: process.env.VERCEL_URL,
         },
       ],
     },
