8a41c97874add2dfc759433ae8feb3a53dee8fa8
mkizka <g.mkizka@gmail.com>
2023-05-05 22:24:00 +0900

    webfinger移動

---
+++ src/app/api/well-known/webfinger/route.ts
@@ -0,0 +1,45 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+import { prisma } from "@/server/prisma";
+import { env } from "@/utils/env";
+
+export async function GET({ nextUrl }: NextRequest) {
+  const resource = nextUrl.searchParams.get("resource");
+  if (
+    !(
+      typeof resource == "string" &&
+      resource.startsWith("acct:") &&
+      resource.endsWith(`@${env.HOST}`)
+    )
+  ) {
+    return NextResponse.json({}, { status: 404 });
+  }
+  const preferredUsername = resource
+    .replace("acct:", "") // startsWithされてるので必ず先頭にある
+    .split("@")[0]; // endsWithされてるので必ず1文字以上ある
+  // TODO: 自ホストのユーザーのみ返す？hostも見る？
+  const user = await prisma.user.findFirst({
+    where: { preferredUsername },
+  });
+  if (!user) {
+    return NextResponse.json({}, { status: 404 });
+  }
+  return NextResponse.json(
+    {
+      subject: resource,
+      links: [
+        {
+          rel: "self",
+          type: "application/activity+json",
+          href: `https://${env.HOST}/users/${user.id}`,
+        },
+      ],
+    },
+    {
+      headers: {
+        "Content-Type": "application/jrd+json",
+      },
+    }
+  );
+}
