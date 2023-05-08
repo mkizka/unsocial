cc2dfa26265a4a266f9c45e5ad117eeed23c9e31
mkizka <g.mkizka@gmail.com>
2023-05-09 02:36:13 +0900

    actionまとめる

--- src/components/NoteForm/action.server.ts
+++
@@ -1,6 +0,0 @@
-import { action as original } from "./action";
-
-export async function action(formData: FormData) {
-  "use server";
-  await original(formData);
-}
--- src/components/NoteForm/component.tsx
+++ src/components/NoteForm/component.tsx
@@ -1,4 +1,12 @@
-import { action } from "./action.server";
+import { action as _action } from "./action";
+
+// "use server"を書いたファイルをテストすると
+// Cannot find module 'private-next-rsc-action-client-wrapper' というエラーが出るため、
+// ここで再定義する
+export async function action(formData: FormData) {
+  "use server";
+  await _action(formData);
+}
 
 export function NoteForm() {
   return (
