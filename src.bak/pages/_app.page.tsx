feb0f99c251cd1ebdee7d83caf8d49181dae51c8
mkizka <g.mkizka@gmail.com>
2023-05-12 00:16:55 +0900

    不要ファイル削除

--- src.bak/pages/_app.page.tsx
+++ src.bak/pages/_app.page.tsx
@@ -4,8 +4,6 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-import { api } from "../utils/api";
-
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
@@ -17,4 +15,4 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;
