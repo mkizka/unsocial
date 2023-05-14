0b3937499717783149459eb48d24fb2e354d91bd
mkizka <g.mkizka@gmail.com>
2023-05-14 14:19:19 +0900

    src.bak削除

--- src.bak/utils/formatZodError.ts
+++
@@ -1,5 +0,0 @@
-import type { z } from "zod";
-
-// Stryker disable next-line ArrowFunction
-export const formatZodError = (error: z.ZodError<unknown>) =>
-  JSON.stringify(error.flatten().fieldErrors);
