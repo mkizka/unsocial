fa87d885904bfc4c22a08779bfce15c807747e8e
mkizka <g.mkizka@gmail.com>
2023-05-14 22:18:09 +0900

    テスト追加

---
+++ src/app/users/[userId]/collections/featured/route.spec.ts
@@ -0,0 +1,62 @@
+import type { Note, User } from "@prisma/client";
+
+import { findOrFetchUserByParams } from "@/utils/findOrFetchUser";
+import { mockedPrisma } from "@/utils/mock";
+
+import { GET } from "./route";
+
+jest.mock("@/utils/findOrFetchUser");
+const mockedFindOrFetchUserByParams = jest.mocked(findOrFetchUserByParams);
+
+describe("/users/[userId]/collections/featured", () => {
+  test("GET", async () => {
+    // arrange
+    mockedFindOrFetchUserByParams.mockResolvedValueOnce({
+      id: "__user__id",
+    } as User);
+    mockedPrisma.note.findMany.mockResolvedValueOnce([
+      {
+        id: "__note__id",
+        userId: "__note__userId",
+        content: "__note__content",
+        createdAt: new Date("2023-01-01"),
+      } as Note,
+    ]);
+    // act
+    const response = await GET({} as Request, { params: { userId: "foo" } });
+    // assert
+    expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toBe(
+      "application/activity+json"
+    );
+    expect(await response.json()).toMatchInlineSnapshot(`
+      {
+        "@context": [
+          "https://www.w3.org/ns/activitystreams",
+        ],
+        "id": "https://myhost.example.com/users/__user__id/collections/featured",
+        "orderedItems": [
+          {
+            "@context": [
+              "https://www.w3.org/ns/activitystreams",
+              "https://w3id.org/security/v1",
+            ],
+            "attributedTo": "https://myhost.example.com/users/__note__userId/activity",
+            "cc": [
+              "https://myhost.example.com/users/__note__userId/followers",
+            ],
+            "content": "__note__content",
+            "id": "https://myhost.example.com/notes/__note__id/activity",
+            "published": "2023-01-01T00:00:00.000Z",
+            "to": [
+              "https://www.w3.org/ns/activitystreams#Public",
+            ],
+            "type": "Note",
+          },
+        ],
+        "totalItems": 1,
+        "type": "OrderedCollection",
+      }
+    `);
+  });
+});
--- src/app/users/[userId]/collections/featured/route.ts
+++ src/app/users/[userId]/collections/featured/route.ts
@@ -21,11 +21,18 @@ export async function GET(
     take: 5,
   });
   // TODO: activityStreams.orderedCollection実装
-  return NextResponse.json({
-    "@context": ["https://www.w3.org/ns/activitystreams"],
-    id: `https://${env.HOST}/users/${user.id}/collections/featured`,
-    type: "OrderedCollection",
-    totalItems: notes.length,
-    orderedItems: notes.map(activityStreams.note),
-  });
+  return NextResponse.json(
+    {
+      "@context": ["https://www.w3.org/ns/activitystreams"],
+      id: `https://${env.HOST}/users/${user.id}/collections/featured`,
+      type: "OrderedCollection",
+      totalItems: notes.length,
+      orderedItems: notes.map(activityStreams.note),
+    },
+    {
+      headers: {
+        "Content-Type": "application/activity+json",
+      },
+    }
+  );
 }
