98707665134c5ce8fcab3bd05bdfec3cc7c9fb36
mkizka <g.mkizka@gmail.com>
2023-05-18 19:22:01 +0900

    元に戻す

--- src/components/Home/parts/LoginButton.tsx
+++ src/components/Home/parts/LoginButton.tsx
@@ -8,18 +8,13 @@ type Props = {
 export function SignInOrOutButton({ isAuthenticated }: Props) {
   if (isAuthenticated) {
     return (
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        data-testid="login-button"
-        onClick={() => signOut()}
-      >
+      <button data-testid="login-button" onClick={() => signOut()}>
         ログアウト
       </button>
     );
   }
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       data-testid="login-button"
       onClick={() => {
         signIn(
