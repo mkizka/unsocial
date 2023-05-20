57fd21a3a0caac471ce510f252ee2ea8e8b5ffa4
mkizka <g.mkizka@gmail.com>
2023-05-20 12:53:17 +0900

    retryをtoPassに

--- e2e/utils/soshal.ts
+++ e2e/utils/soshal.ts
@@ -1,29 +1,14 @@
 import type { Page } from "@playwright/test";
 import { expect } from "@playwright/test";
 
-const retry = async (times: number, func: (i?: number) => Promise<void>) => {
-  for (const i of [...Array(times).keys()]) {
-    try {
-      await func(i);
-      break;
-    } catch (e) {
-      if (i === times - 1) {
-        throw e;
-      }
-    }
-  }
-};
-
 export const login = async (page: Page) => {
   // まれに /api/auth/signin?csrf=true にリダイレクトされることがあるのでリトライ
-  await retry(3, async () => {
+  await expect(async () => {
     await page.goto("/");
     await page.locator("[data-testid=login-button]").click();
     await page.waitForURL((url) => url.pathname != "/");
-    if (new URL(page.url()).pathname != "/api/auth/verify-request") {
-      throw new Error();
-    }
-  });
+    expect(page.url()).toContain("/api/auth/verify-request");
+  }).toPass();
   await page.goto("http://localhost:8025");
   await page.locator(".msglist-message").first().click();
   const signInUrl = await page
