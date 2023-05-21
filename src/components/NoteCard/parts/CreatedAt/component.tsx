ced4f74d7ccc57f065fbd6b17e939f7cff0df3e4
mkizka <g.mkizka@gmail.com>
2023-05-21 16:19:36 +0900

    ハイドレーション不一致のエラー解消

--- src/components/NoteCard/parts/CreatedAt/component.tsx
+++ src/components/NoteCard/parts/CreatedAt/component.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 function format(date: Date) {
-  let diff = Math.ceil((new Date().getTime() - date.getTime()) / 1000);
+  let diff = Math.floor((new Date().getTime() - date.getTime()) / 1000);
   if (diff <= 60) return `${diff}秒前`;
-  diff = Math.ceil(diff / 60);
+  diff = Math.floor(diff / 60);
   if (diff <= 60) return `${diff}分前`;
-  diff = Math.ceil(diff / 60);
+  diff = Math.floor(diff / 60);
   if (diff <= 24) return `${diff}時間前`;
-  diff = Math.ceil(diff / 24);
+  diff = Math.floor(diff / 24);
   return date.toLocaleDateString();
 }
 
@@ -20,9 +21,16 @@ export function CreatedAt({
   href: string;
   createdAt: Date;
 }) {
+  const [createdAtText, setCreatedAtText] = useState(format(createdAt));
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCreatedAtText(format(createdAt));
+    }, 30000);
+    return () => clearInterval(timer);
+  });
   return (
     <Link data-testid="note-card__link" href={href} className="hover:underline">
-      {format(createdAt)}
+      {createdAtText}
     </Link>
   );
 }
