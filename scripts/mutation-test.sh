4c283ccdf6ff23b4e7337963a97dcb7d72595623
mkizka <g.mkizka@gmail.com>
2023-05-23 02:06:47 +0900

    ミューテーションテスト結果をアップロード (#68)

--- scripts/mutation-test.sh
+++ scripts/mutation-test.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/bash
 
-if [ -z $BRANCH_NAME ]; then
+if [ -z "$BRANCH_NAME" ]; then
   BRANCH_NAME=$(git branch --show-current)
 fi
-rm -rf reports
-mkdir -p reports
+MUTATION_DIR=reports/mutation
+CACHE_PATH=$MUTATION_DIR/stryker-incremental.json
+
+rm -rf $MUTATION_DIR
+mkdir -p $MUTATION_DIR
+
 echo "$BRANCH_NAME のキャッシュファイルをダウンロード..."
-curl -f --progress-bar -o reports/stryker-incremental.json "https://minio-s3.paas.mkizka.dev/soshal-gha/mutation-test/$BRANCH_NAME/stryker-incremental.json"
-if [ ! -f reports/stryker-incremental.json ]; then
+curl -f --progress-bar -o $CACHE_PATH "https://minio-s3.paas.mkizka.dev/soshal-gha/mutation-test/$BRANCH_NAME/stryker-incremental.json"
+if [ ! -f $CACHE_PATH ]; then
   echo "main のキャッシュファイルをダウンロード..."
-  curl -f --progress-bar -o reports/stryker-incremental.json https://minio-s3.paas.mkizka.dev/soshal-gha/mutation-test/main/stryker-incremental.json
+  curl -f --progress-bar -o $CACHE_PATH https://minio-s3.paas.mkizka.dev/soshal-gha/mutation-test/main/stryker-incremental.json
 fi
-if [ ! -f reports/stryker-incremental.json ]; then
+if [ ! -f $CACHE_PATH ]; then
   echo "キャッシュファイルが取得できませんでした"
   exit 1;
 fi
+
 pnpm stryker run "$@"
