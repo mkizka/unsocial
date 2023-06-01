4e04167d5b0fcf25fc5b807e7744d2db47f70660
mkizka <g.mkizka@gmail.com>
2023-06-02 00:50:37 +0900

    eslint設定をpackagesに切り出す

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,18 +1,7 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  root: true,
   parserOptions: {
-    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint", "unused-imports", "simple-import-sort"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/consistent-type-imports": "error",
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-  },
+  extends: ["@soshal", "next/core-web-vitals"],
 };
