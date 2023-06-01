90a304c449f85c1656612898c9ebe0fddbdfc5f2
mkizka <g.mkizka@gmail.com>
2023-06-02 01:01:52 +0900

    @soshal/database追加

--- apps/web/src/server/prisma.ts
+++ apps/web/src/server/prisma.ts
@@ -1,14 +1,8 @@
 // Stryker disable all
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@soshal/database";
 
 import { env } from "@/utils/env";
-import { globalize } from "@/utils/globalize";
 
-export const prisma = globalize(
-  "prisma",
-  () =>
-    new PrismaClient({
-      log:
-        env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    })
-);
+export const prisma = new PrismaClient({
+  log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
