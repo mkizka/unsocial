0e3aee7d49c8a6056232325d296443f33dad62b1
mkizka <g.mkizka@gmail.com>
2023-06-03 13:42:14 +0900

    fix date

--- apps/web/src/server/background/runners/relay.spec.ts
+++ apps/web/src/server/background/runners/relay.spec.ts
@@ -4,7 +4,7 @@ import { mockedKeys } from "@/utils/httpSignature/__fixtures__/keys";
 import { relayActivity } from "./relay";
 
 jest.useFakeTimers();
-jest.setSystemTime(new Date("2020-01-01T00:00:00"));
+jest.setSystemTime(new Date("2020-01-01T00:00:00Z"));
 
 jest.mock("@/utils/fetcher");
 const mockedFetcher = jest.mocked(fetcher);
@@ -27,10 +27,10 @@ describe("relayActivity", () => {
           "body": "{"type":"Dummy"}",
           "headers": {
             "Accept": "application/activity+json",
-            "date": "Tue, 31 Dec 2019 15:00:00 GMT",
+            "date": "Wed, 01 Jan 2020 00:00:00 GMT",
             "digest": "SHA-256=GJpn7Z0rhlxi7/yIwjEeQln6ix6FqsnOBpzQ156l7mM=",
             "host": "misskey.localhost",
-            "signature": "keyId="https://myhost.example.com/users/dummy#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest",signature="aHxQGySL2CrZVZJcmDhyhtpsFcYrC9m5mhFiinC/3HRMz1X+627Sw2cwl+rPqb9ZFfjii3fzlthqJv98PQEfIODt+UPtafEFMEccHMbeCNL7peHs4EvBn2P4vna6mAYhGExljTpovrRZGLqdqyGN4ebJ6kpBGF5rXqpb5ndQ5meeVozIRRx5yZxCjpKYpekRzKBIjzARJMoth+FqBxxUXriQ2Znc7/mPHmzjPErxc+VqKU9ZQ1f1YTEKC17s45vgFKitcYrCvIumFk+9aHyp45CobzkaMsTJZuv+B9/tXdqHv3mXaq7lUAZnUX9CmqM7prps0PJQM3Z4KStBjQ=="",
+            "signature": "keyId="https://myhost.example.com/users/dummy#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest",signature="PWVjjwtTTcPxnTHwIHMBYwuzPC+jiO7UdDrMyCDF2ELMxFIsdT2x+0YHO7gZzxElrFiwrFoaDLcjoW2CS7mreEUgQoT76Z5K/BwcJB1335Q3OU1F9sybeyziAB1+Qn7bajuXDctYoAb/1p7cOS+Vlc2fpecgU0RTzFZ8f0wEi3wVXQ3BXxxg3uvlTEjZXza5dh6Ia9rDAd+PAl/LJIkQubklXIibfi9SelHkdXJKwxYXZmwuZ13vy1kmzVBzGS3CtYR0qoeBaAYfUmZS1wHD+PIRjeIHzGqtN5kV4uNypm5/6pxLbz+awQPVnweuS4gGbnOJzkbLuZbORRoD3g=="",
           },
           "method": "POST",
         },
