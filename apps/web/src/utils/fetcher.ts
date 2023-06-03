a2bc392f9ffee53437d22faf3c2ee150c2c06058
mkizka <g.mkizka@gmail.com>
2023-06-03 13:46:10 +0900

    fix

--- apps/web/src/utils/fetcher.ts
+++ apps/web/src/utils/fetcher.ts
@@ -3,15 +3,19 @@ import { logger } from "./logger";
 
 type FetchArgs = Parameters<typeof fetch>;
 
-const defaultOptions: FetchArgs[1] = {
-  headers: {
-    "Content-Type": "application/json",
-  },
+const defaultHeader: HeadersInit = {
+  "Content-Type": "application/json",
 };
 
 export const fetcher = async (url: FetchArgs[0], init?: FetchArgs[1]) => {
   try {
-    const response = await fetch(url, { ...defaultOptions, ...init });
+    const response = await fetch(url, {
+      ...init,
+      headers: {
+        ...defaultHeader,
+        ...init?.headers,
+      },
+    });
     if (!response.ok) {
       logger.info(`${response.status}: ${response.url}`);
     }
