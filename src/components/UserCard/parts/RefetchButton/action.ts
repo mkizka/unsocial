abda6c2c26bc5e98fa600303c1583bf9c650548e
mkizka <g.mkizka@gmail.com>
2023-06-19 22:36:53 +0900

    連打防止

--- src/components/UserCard/parts/RefetchButton/action.ts
+++ src/components/UserCard/parts/RefetchButton/action.ts
@@ -1,8 +1,18 @@
+import { logger } from "@/utils/logger";
 import { prisma } from "@/utils/prisma";
 
 export async function action(userId: string) {
-  await prisma.user.update({
-    where: { id: userId },
+  const { count } = await prisma.user.updateMany({
+    where: {
+      id: userId,
+      // 連打されたときにリクエストが連続しないように、10秒以上経過したユーザーのみ更新する
+      lastFetchedAt: {
+        lte: new Date(Date.now() - 1000 * 10),
+      },
+    },
     data: { lastFetchedAt: null },
   });
+  if (count == 0) {
+    logger.info("ユーザー情報の再取得が行われませんでした");
+  }
 }
