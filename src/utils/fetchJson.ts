f10f8f2c6372ec70b70129eae78b27fc7f5eb9be
mkizka <g.mkizka@gmail.com>
2023-06-21 20:57:42 +0900

    レスポンスがJSONじゃなかった時エラーにしない

--- src/utils/fetchJson.ts
+++ src/utils/fetchJson.ts
@@ -3,24 +3,32 @@ import { createLogger } from "./logger";
 
 const logger = createLogger("fetchJson");
 
-export const fetchJson = (...[input, init]: Parameters<typeof fetch>) => {
-  // mswはURLインスタンスで渡されたリクエストをモック出来ない？
-  return fetch(input.toString(), {
-    ...init,
-    headers: {
-      ...init?.headers,
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        logger.warn(`HTTPエラー(${input}): ${response.status}`);
-        return null;
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      logger.warn(`ネットワークエラー(${input}): ${error}`);
-      return null;
+const jsonOrText = async (response: Response) => {
+  const body = await response.text();
+  try {
+    return JSON.parse(body);
+  } catch {
+    return body;
+  }
+};
+
+export const fetchJson = async (...[input, init]: Parameters<typeof fetch>) => {
+  try {
+    // mswはURLインスタンスで渡されたリクエストをモック出来ない？
+    const response = await fetch(input.toString(), {
+      ...init,
+      headers: {
+        ...init?.headers,
+        "Content-Type": "application/json",
+      },
     });
+    if (!response.ok) {
+      logger.warn(`HTTPエラー(${input}): ${response.status}`);
+      return null;
+    }
+    return await jsonOrText(response);
+  } catch (error) {
+    logger.warn(`fetchエラー(${input}): ${error}`);
+    return null;
+  }
 };
