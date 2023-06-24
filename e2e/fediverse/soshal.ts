76c37290bcbee7a78ab2e7655dde22849a655be9
mkizka <g.mkizka@gmail.com>
2023-06-25 00:51:31 +0900

    fix

--- e2e/fediverse/soshal.ts
+++ e2e/fediverse/soshal.ts
@@ -8,7 +8,6 @@ export class MyhostSoshalHandler extends FediverseHandler {
 
   async login() {
     await this.goto("/auth");
-    await this.page.getByTestId("text-input-name").fill("テスト");
     await this.page.getByTestId("text-input-preferredUsername").fill("test");
     await this.page.getByTestId("password-input").fill("testtest");
     await this.page.getByTestId("submit-button").click();
