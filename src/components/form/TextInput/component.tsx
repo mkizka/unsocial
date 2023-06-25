246eda53da0e59287b7dff70be5ebef6f0a410ba
mkizka <g.mkizka@gmail.com>
2023-06-25 21:32:41 +0900

    tailwindcss/formsのスタイル自動追加やめる

--- src/components/form/TextInput/component.tsx
+++ src/components/form/TextInput/component.tsx
@@ -4,7 +4,7 @@ export function TextInput(props: ComponentProps<"input">) {
   return (
     <input
       type="text"
-      className="block w-full rounded shadow
+      className="form-input block w-full rounded shadow
       bg-primary-light border-primary-dark
       focus:border-primary-dark focus:ring focus:ring-primary-dark focus:ring-opacity-50"
       {...props}
