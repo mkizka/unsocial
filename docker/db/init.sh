58a2892c3cb747a9a68ed0f76ae5f71d4c8be65e
mkizka <g.mkizka@gmail.com>
2023-07-03 00:22:47 +0900

    fix db

---
+++ docker/db/init.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+createdb -U postgres mastodon
+createdb -U postgres misskey
+pg_restore -U postgres -d mastodon /docker-entrypoint-initdb.d/dumps/mastodon.dump
+pg_restore -U postgres -d misskey /docker-entrypoint-initdb.d/dumps/misskey.dump
