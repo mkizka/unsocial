ae1c936fb93b59be0229940873950ade09b297a4
mkizka <g.mkizka@gmail.com>
2023-07-11 02:02:49 +0900

    リファクタ

--- e2e/federation.test.ts
+++ e2e/federation.test.ts
@@ -14,35 +14,76 @@ type RunTestParams = {
   to: FediverseHandler;
 };
 
+type Step = [string, () => Promise<void>];
+
 // fromからtoへ投稿が連合するシナリオ
 const runTest = async ({ from, to }: RunTestParams) => {
   const content = crypto.randomUUID();
-  await test.step(`${from.domain}: ログイン`, () => from.login());
-  await test.step(`${to.domain}: ログイン`, () => to.login());
-  await test.step(`${from.domain}: ユーザーを確認`, () =>
-    from.expectedUser(to.user));
-  await test.step(`${to.domain}: ユーザーを確認`, () =>
-    to.expectedUser(from.user));
-  await test.step(`${to.domain}: ${from.user}をフォロー`, () =>
-    to.followAndWait(from.user));
-  await test.step(`${from.domain}: フォローされたことを確認`, () =>
-    from.expectFollowed(to.user));
-  await test.step(`${to.domain}: フォローできたことを確認`, () =>
-    to.expectFollowing(from.user));
-  await test.step(`${from.domain}: 投稿`, () => from.postNoteAndWait(content));
-  await test.step(`${to.domain}: 投稿が連合されたことを確認`, () =>
-    to.expectPosted(content));
-  await test.step(`${to.domain}: いいね`, () => to.likeAndWait(content));
-  await test.step(`${from.domain}: いいねされたことを確認`, () =>
-    from.expectLiked(content, to.user));
-  await test.step(`${from.domain}: 投稿を削除`, () =>
-    from.deleteAndWait(content));
-  await test.step(`${to.domain}: 削除されたことを確認`, () =>
-    to.expectDeleted(content));
-  await test.step(`${to.domain}: フォロー解除`, () =>
-    to.unfollowAndWait(from.user));
-  await test.step(`${from.domain}: フォロー解除されたことを確認`, () =>
-    from.expectNotFollowed(to.user));
+  const steps: Step[] = [
+    [
+      `${from.domain}: ログイン`, //
+      () => from.login(),
+    ],
+    [
+      `${to.domain}: ログイン`, //
+      () => to.login(),
+    ],
+    [
+      `${from.domain}: ユーザーを確認`, //
+      () => from.expectedUser(to.user),
+    ],
+    [
+      `${to.domain}: ユーザーを確認`, //
+      () => to.expectedUser(from.user),
+    ],
+    [
+      `${to.domain}: ${from.user}をフォロー`, //
+      () => to.followAndWait(from.user),
+    ],
+    [
+      `${from.domain}: フォローされたことを確認`,
+      () => from.expectFollowed(to.user),
+    ],
+    [
+      `${to.domain}: フォローできたことを確認`,
+      () => to.expectFollowing(from.user),
+    ],
+    [
+      `${from.domain}: 投稿`, //
+      () => from.postNoteAndWait(content),
+    ],
+    [
+      `${to.domain}: 投稿が連合されたことを確認`, //
+      () => to.expectPosted(content),
+    ],
+    [
+      `${to.domain}: いいね`, //
+      () => to.likeAndWait(content),
+    ],
+    [
+      `${from.domain}: いいねされたことを確認`,
+      () => from.expectLiked(content, to.user),
+    ],
+    [
+      `${from.domain}: 投稿を削除`, //
+      () => from.deleteAndWait(content),
+    ],
+    [
+      `${to.domain}: 削除されたことを確認`, //
+      () => to.expectDeleted(content),
+    ],
+    [
+      `${to.domain}: フォロー解除`, //
+      () => to.unfollowAndWait(from.user),
+    ],
+    [
+      `${from.domain}: フォロー解除されたことを確認`,
+      () => from.expectNotFollowed(to.user),
+    ],
+  ];
+  for (const [label, action] of steps) {
+    await test.step(label, action);
+  }
 };
 
 test.describe("Federation", () => {
