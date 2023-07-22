30926173e78367fd032dbbe9b7d63b35796890de
mkizka <g.mkizka@gmail.com>
2023-07-23 00:24:59 +0900

    e2eの使っていない設定を削除

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,6 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 import { defineConfig, devices } from "@playwright/test";
 
-const chromium = {
-  ...devices["Desktop Chrome"],
-  locale: "ja-JP",
-};
-
 const config: PlaywrightTestConfig = {
   testDir: "e2e",
   outputDir: "reports/.e2e",
@@ -13,14 +8,11 @@ const config: PlaywrightTestConfig = {
   reporter: [["html", { open: "never", outputFolder: "reports/e2e" }]],
   projects: [
     {
-      name: "setup",
-      use: chromium,
-      testMatch: "e2e/auth.setup.ts",
-    },
-    {
       name: "chromium",
-      use: chromium,
-      dependencies: ["setup"],
+      use: {
+        ...devices["Desktop Chrome"],
+        locale: "ja-JP",
+      },
     },
   ],
   use: {
