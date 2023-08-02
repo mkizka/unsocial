6b9fb26f942668017e0ab9b2a13632783fc0ea78
mkizka <g.mkizka@gmail.com>
2023-08-03 00:53:18 +0900

    update

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,6 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 set -eu
 
 docker compose up -d
-pnpm prisma db push
+pnpm prisma db push --skip-generate
 NODE_EXTRA_CA_CERTS=$(pwd)/docker/mkcert/rootCA.pem pnpm next
