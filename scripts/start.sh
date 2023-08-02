5a92ba4bf9560db3a583ab6ad5c4745cbbca5ff2
mkizka <g.mkizka@gmail.com>
2023-08-03 01:43:37 +0900

    update

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,13 @@
 set -eu
 
 pnpm prisma db push --skip-generate
-NEXTAUTH_URL=https://$RAILWAY_STATIC_URL pnpm next start -p "$PORT"
+if [ -n "$RAILWAY_STATIC_URL" ]; then
+  NEXTAUTH_URL="https://$RAILWAY_STATIC_URL"
+  export NEXTAUTH_URL
+fi
+# prデプロイではデータベースを分ける
+if [ "$RAILWAY_ENVIRONMENT" = "pr" ]; then
+  DATABASE_URL="${DATABASE_URL///railway//$RAILWAY_GIT_BRANCH}"
+  export DATABASE_URL
+fi
+pnpm next start -p "$PORT"
