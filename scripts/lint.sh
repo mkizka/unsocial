e9296e4fa33fffd0bacc7dd69bc1bdfc4d584d25
mkizka <g.mkizka@gmail.com>
2023-08-05 14:25:23 +0900

    lint改善

---
+++ scripts/lint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+set -eu
+
+rm -rf .next/types
+pnpm tsc
+
+if [ "${1:-}" = "--fix" ]; then
+  pnpm shellcheck -f diff scripts/*.sh | git apply
+  pnpm next lint --fix
+  pnpm prettier . --check --write
+else
+  pnpm shellcheck scripts/*.sh
+  pnpm next lint
+  pnpm prettier . --check
+fi
