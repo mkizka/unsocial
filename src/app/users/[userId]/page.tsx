5d7d19edd61a7e4da1a199f5320e6e8f6de66d80
mkizka <g.mkizka@gmail.com>
2023-08-11 17:50:35 +0900

    winstonはがしてみる

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 
 import { Timeline } from "@/components/Timeline";
 import { UserCard } from "@/components/UserCard";
-import { noteService, userService } from "@/server/service";
+import { userService } from "@/server/service";
 
 export default async function UserPage({
   params,
@@ -13,11 +13,12 @@ export default async function UserPage({
   if (!user) {
     notFound();
   }
-  const notes = await noteService.findManyNoteCardsByUserId(user.id);
+  // TODO: 修正
+  // const notes = await noteService.findManyNoteCardsByUserId(user.id);
   return (
     <>
       <UserCard user={user} />
-      <Timeline notes={notes} />
+      <Timeline />
     </>
   );
 }
