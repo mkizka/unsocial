2d913b7e0411d0b6f225bdca27a6a1959b0ac30c
mkizka <g.mkizka@gmail.com>
2023-08-11 17:34:17 +0900

    wip

--- src/components/Home/UserHome.tsx
+++ src/components/Home/UserHome.tsx
@@ -1,7 +1,5 @@
 import type { Session } from "next-auth";
 
-import { noteService } from "@/server/service";
-
 import { NoteForm } from "../NoteForm";
 import { Timeline } from "../Timeline";
 import { SignOutButton } from "./parts/LoginButton";
@@ -11,13 +9,12 @@ type Props = {
 };
 
 export async function UserHome({ user }: Props) {
-  const notes = await noteService.findManyNoteCards();
   return (
     <main>
       <p data-testid="is-logged-in">{user.id}でログイン中</p>
       <SignOutButton />
       <NoteForm />
-      <Timeline notes={notes} />
+      <Timeline />
     </main>
   );
 }
--- src/components/Timeline/component.tsx
+++ src/components/Timeline/component.tsx
@@ -1,15 +1,29 @@
-import type { noteService } from "@/server/service";
+"use client";
+import useSWRInfinite from "swr/infinite";
+
+import type { TimelineResponse } from "@/app/api/timeline/route";
 
 import { NoteCard } from "../NoteCard";
 
-type Props = {
-  notes: noteService.NoteCard[];
+const getKey = (index: number, previousPageData: TimelineResponse[]) => {
+  if (previousPageData && !previousPageData.length) return null; // reached the end
+  return `/api/notes?page=${index + 1}`;
 };
 
-export function Timeline({ notes }: Props) {
+const fetcher = (url: string) =>
+  fetch(url).then(
+    // TODO: 型見直す
+    (res) => res.json() as unknown as Promise<TimelineResponse>,
+  );
+
+export function Timeline() {
+  const { data: notes, error } = useSWRInfinite(getKey, fetcher);
+  if (error) return <div>failed to load</div>;
+  if (!notes) return <div>loading...</div>;
+
   return (
     <div>
-      {notes.map((note) => (
+      {notes.flat().map((note) => (
         <NoteCard key={note.id} note={note} />
       ))}
     </div>
