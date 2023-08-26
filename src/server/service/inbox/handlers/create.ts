166d8a1b08d60810bd1d7738746c309dec439d89
mkizka <g.mkizka@gmail.com>
2023-08-26 12:49:48 +0900

    urlが重複するノートが来たらスキップ
    close #382

--- src/server/service/inbox/handlers/create.ts
+++ src/server/service/inbox/handlers/create.ts
@@ -1,16 +1,27 @@
 import { noteRepository } from "@/server/repository";
 import { inboxCreateSchema } from "@/server/schema/create";
+import { createLogger } from "@/utils/logger";
 
 import { ActivitySchemaValidationError } from "../errors";
 import { type InboxHandler } from "./shared";
+
+const logger = createLogger("inboxCreateService");
 
 export const handle: InboxHandler = async (activity, actorUser) => {
   const parsedNote = inboxCreateSchema.safeParse(activity);
   if (!parsedNote.success) {
     return new ActivitySchemaValidationError(parsedNote.error, activity);
   }
-  await noteRepository.createFromActivity({
-    activity: parsedNote.data.object,
-    userId: actorUser.id,
-  });
+  await noteRepository
+    .createFromActivity({
+      activity: parsedNote.data.object,
+      userId: actorUser.id,
+    })
+    .catch((error) => {
+      // https://www.prisma.io/docs/reference/api-reference/error-reference#p2002
+      if (error.code === "P2002") {
+        logger.info("すでに存在するノートを受信したのでスキップします");
+        return;
+      }
+    });
 };
