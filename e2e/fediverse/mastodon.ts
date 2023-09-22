5e8d390d957cbe7262e7fb220025a2fee0363425
mkizka <g.mkizka@gmail.com>
2023-09-22 16:56:48 +0900

    fix

--- e2e/fediverse/mastodon.ts
+++ e2e/fediverse/mastodon.ts
@@ -18,9 +18,8 @@ export class MastodonHandler extends FediverseHandler {
     await this.goto("/");
     await this.page.locator(".search__input").fill(user);
     await this.page.locator(".search__input").press("Enter");
-    await expect(
-      this.page.locator(".display-name__account", { hasText: user }),
-    ).toBeVisible();
+    await this.page.waitForURL("/search");
+    await expect(this.page.locator(`text=${user}`)).toBeVisible();
   }
 
   getNote(content: string) {
