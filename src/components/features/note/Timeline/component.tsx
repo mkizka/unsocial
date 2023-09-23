8ade26212b7884798dec9ca1ccf9cee12fb55dd3
mkizka <g.mkizka@gmail.com>
2023-09-23 19:58:45 +0900

    ユーザーページにタイムライン追加 (#502)

--- src/components/features/note/Timeline/component.tsx
+++ src/components/features/note/Timeline/component.tsx
@@ -1,5 +1,20 @@
+import { notFound } from "next/navigation";
+
+import { userService } from "@/server/service";
+
 import { TimelineLoader } from "./TimelineLoader";
 
-export async function Timeline() {
-  return <TimelineLoader />;
+type Props = {
+  userKey?: string;
+};
+
+export async function Timeline({ userKey }: Props) {
+  if (!userKey) {
+    return <TimelineLoader />;
+  }
+  const user = await userService.findOrFetchUserByKey(userKey);
+  if (user instanceof Error) {
+    notFound();
+  }
+  return <TimelineLoader userId={user.id} />;
 }
