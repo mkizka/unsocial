6e81bbe6cf6f939a20700362033f37d4a7b4ed02
mkizka <g.mkizka@gmail.com>
2023-10-13 09:38:22 +0900

    experimental外す

--- src/components/features/note/NoteForm/parts/SubmitButton/component.tsx
+++ src/components/features/note/NoteForm/parts/SubmitButton/component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 import { Spinner } from "@/components/clients/Spinner";
 
