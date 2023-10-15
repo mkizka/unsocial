40db3147f25333a910b9d463f77fb4acb08bbf2f
mkizka <g.mkizka@gmail.com>
2023-10-15 18:54:28 +0900

    fix

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -eu
 
-docker compose up -d
+docker compose --profile unsocial up -d
 pnpm prisma db push --skip-generate
 pnpm seed
 NODE_EXTRA_CA_CERTS=$(pwd)/docker/mkcert/rootCA.pem pnpm next
