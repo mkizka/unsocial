2f0ee4afa700665b4f4bbc9990dcd7704d6e1526
mkizka <g.mkizka@gmail.com>
2023-10-21 16:42:06 +0900

    fix

--- src/components/features/note/NoteCard/parts/LikeButton/component.tsx
+++ src/components/features/note/NoteCard/parts/LikeButton/component.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { HeartIcon as UnLikedIcon } from "@heroicons/react/24/outline";
 import { HeartIcon as LikedIcon } from "@heroicons/react/24/solid";
-import { experimental_useOptimistic as useOptimistic } from "react";
+import { useOptimistic } from "react";
 
 import { action } from "./action";
 
