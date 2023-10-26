5711e0623d6e94c61a24f8d3a0d450d77da82681
mkizka <g.mkizka@gmail.com>
2023-10-27 00:10:17 +0900

    fix style

--- src/components/clients/Header/component.tsx
+++ src/components/clients/Header/component.tsx
@@ -2,7 +2,7 @@ import { SearchModal } from "@/components/clients/SearchModal";
 
 export function Header() {
   return (
-    <header className="fixed z-10 flex h-[54px] w-full items-center bg-primary-dark/75 pl-8">
+    <header className="fixed z-10 flex h-[54px] w-full items-center rounded-b-md bg-primary-light pl-4 shadow-md">
       <SearchModal />
     </header>
   );
