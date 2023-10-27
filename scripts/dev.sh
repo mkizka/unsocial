b570e95c0e122b1fad945f4e638ee95319c059a1
mkizka <g.mkizka@gmail.com>
2023-10-27 16:33:41 +0900

    turboやめる

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,4 +4,4 @@ set -eu
 docker compose --profile "${1:-unsocial}" up -d
 pnpm prisma db push --skip-generate
 pnpm -s seed
-NODE_EXTRA_CA_CERTS=$(pwd)/docker/mkcert/rootCA.pem pnpm next dev --turbo
+NODE_EXTRA_CA_CERTS=$(pwd)/docker/mkcert/rootCA.pem pnpm next
