4aaadfbf10819e30b111542ee614f321d8591a95
mkizka <g.mkizka@gmail.com>
2023-10-27 16:52:21 +0900

    検索フォーム追加

--- src/components/clients/SearchModal/component.tsx
+++ src/components/clients/SearchModal/component.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
 
 import { useSearchModal } from "./hooks";
 
 export function SearchModal() {
   const searchModal = useSearchModal();
+  const router = useRouter();
   return (
     <>
       <button onClick={searchModal.open}>
@@ -12,9 +14,39 @@ export function SearchModal() {
       </button>
       {searchModal.isOpen && (
         <div
-          className="fixed left-0 top-0 z-50 h-screen w-screen cursor-pointer bg-black bg-opacity-70"
-          onClick={searchModal.close}
-        />
+          className="fixed left-0 top-0 z-50 flex h-screen w-screen cursor-pointer items-center justify-center bg-black bg-opacity-70"
+          onClick={(e) => {
+            // 背景をクリックしたときだけモーダルを閉じる
+            if (e.target !== e.currentTarget) return;
+            searchModal.close();
+          }}
+        >
+          <div className="h-16 w-[500px] bg-primary shadow-2xl">
+            <form
+              action={(formData) => {
+                const input = formData.get("input");
+                if (typeof input === "string" && input.startsWith("@")) {
+                  searchModal.close();
+                  router.push(`/${input}`);
+                  return;
+                }
+                alert("@から始まる必要があります");
+              }}
+              className="flex h-full w-full items-center justify-between px-4"
+            >
+              <input
+                type="text"
+                name="input"
+                className="h-full w-full bg-transparent text-dark focus:outline-none"
+                placeholder="@name@example.com"
+                autoFocus
+              />
+              <button type="submit" className="h-7 w-7 text-dark">
+                <MagnifyingGlassIcon className="h-7 w-7 text-dark hover:opacity-70" />
+              </button>
+            </form>
+          </div>
+        </div>
       )}
     </>
   );
