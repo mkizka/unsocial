b3cefc7d9e1376ce66f7e1e8b57fc90dedd1fef3
mkizka <g.mkizka@gmail.com>
2023-11-11 00:22:35 +0900

    fix

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,12 @@ import "@/app/globals.css";
 
 import type { Preview } from "@storybook/react";
 
-const preview: Preview = {};
+const preview: Preview = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
 
 export default preview;
