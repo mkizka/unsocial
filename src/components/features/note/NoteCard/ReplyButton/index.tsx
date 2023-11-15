4919e686029e94f18d590a6fab5aaef71083fc52
mkizka <g.mkizka@gmail.com>
2023-11-16 00:22:59 +0900

    fix

--- src/components/features/note/NoteCard/ReplyButton/index.tsx
+++ src/components/features/note/NoteCard/ReplyButton/index.tsx
@@ -2,30 +2,15 @@
 
 import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
 
 type Props = {
   url: string;
 };
 
-function ReplyIcon() {
-  return (
-    <ChatBubbleLeftIcon className="h-5 w-5 text-dark transition-colors hover:text-gray" />
-  );
-}
-
 export function ReplyButton({ url }: Props) {
-  const searchParams = useSearchParams();
-  if (searchParams.has("reply")) {
-    return (
-      <button type="button">
-        <ReplyIcon />
-      </button>
-    );
-  }
   return (
     <Link href={url + "?reply"}>
-      <ReplyIcon />
+      <ChatBubbleLeftIcon className="h-5 w-5 text-dark transition-colors hover:text-gray" />
     </Link>
   );
 }
