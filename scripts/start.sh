a14feb7e50f96da610ddd7ea028e0fa6d2deced2
mkizka <g.mkizka@gmail.com>
2023-11-23 17:29:05 +0900

    lint修正

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@ set -eu
 
 PRISMA_VERSION=$(jq -r '.devDependencies.prisma' package.json)
 corepack enable pnpm
-for i in {1..5}; do
+for _ in {1..5}; do
   pnpm -s dlx "prisma@$PRISMA_VERSION" db push --skip-generate && break
   echo "Retrying..."
   sleep 1
