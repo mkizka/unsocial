040395c57ebe7a529f279062c89823998ae722ed
mkizka <g.mkizka@gmail.com>
2023-12-02 16:13:59 +0900

    NotePage移動

--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -1,16 +1,33 @@
-import { NotePage } from "@/components/pages/NotePage";
+import { notFound } from "next/navigation";
+import { Fragment } from "react";
 
-export default function Page({
+import { NoteCard } from "@/components/features/note/NoteCard";
+import { NoteForm } from "@/components/features/note/NoteForm";
+import { UserList } from "@/components/ui/UserList";
+import { noteService } from "@/server/service";
+
+export default async function Page({
   params,
   searchParams,
 }: {
   params: { noteId: string };
   searchParams: { reply?: string };
 }) {
+  const note = await noteService.findUniqueNoteCard(params.noteId);
+  if (!note) {
+    notFound();
+  }
   return (
-    <NotePage
-      noteId={params.noteId}
-      shouldShowReplyForm={"reply" in searchParams}
-    />
+    <div className="space-y-1">
+      {note.replyTo && <NoteCard note={note.replyTo} />}
+      <NoteCard note={note} showDetail />
+      <UserList users={note.likes.map((like) => like.user)} />
+      {"reply" in searchParams && <NoteForm replyToId={note.id} autoFocus />}
+      {note.replies.map((reply) => (
+        <Fragment key={reply.id}>
+          <NoteCard note={reply} withReplyLink={reply.replies.length > 0} />
+        </Fragment>
+      ))}
+    </div>
   );
 }
