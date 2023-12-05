c34d0c44596e77066673b017e771dd2c173fe5c4
mkizka <g.mkizka@gmail.com>
2023-12-06 01:25:23 +0900

    fix

--- src/server/repository/index.ts
+++ src/server/repository/index.ts
@@ -2,5 +2,4 @@ export * as credentialRepository from "./credential";
 export * as followRepository from "./follow";
 export * as likeRepository from "./like";
 export * as noteRepository from "./note";
-export * as s3Repository from "./s3";
 export * as userRepository from "./user";
