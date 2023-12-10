ce670e92179b76ce2a9e18218140e67dc30ba567
mkizka <g.mkizka@gmail.com>
2023-12-10 22:34:06 +0900

    e2eでもseedを使う

--- scripts/setup-for-e2e.sh
+++ scripts/setup-for-e2e.sh
@@ -4,6 +4,7 @@ set -eu
 docker compose --profile "$1" up -d
 
 pnpm dotenv -e e2e/.env.myhost -- pnpm prisma db push --skip-generate
+pnpm dotenv -e e2e/.env.myhost -- ./scripts/seed.ts
 E2E_DIST_DIR=.next/e2e/myhost pnpm build
 
 if [ "$1" = "unsocial" ]; then
