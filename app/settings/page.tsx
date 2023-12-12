90dd6be5216a644a9c888778148d0db475f0cbc9
mkizka <g.mkizka@gmail.com>
2023-12-12 22:59:50 +0900

    設定画面にログアウト追加 (#931)

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,14 +1,23 @@
 import { Card } from "@/_shared/components/ui/Card";
 
 import { IconSettingsForm } from "./_components/IconSettingsForm";
+import { SignOutForm } from "./_components/SignoutForm";
 
 export default function Page() {
   return (
-    <section className="space-y-1">
-      <Card>
-        <h1 className="font-bold">プロフィール設定</h1>
-      </Card>
-      <IconSettingsForm />
-    </section>
+    <div className="space-y-1">
+      <section className="space-y-1">
+        <Card>
+          <h1 className="font-bold">プロフィール</h1>
+        </Card>
+        <IconSettingsForm />
+      </section>
+      <section className="space-y-1">
+        <Card>
+          <h1 className="font-bold">アカウント</h1>
+        </Card>
+        <SignOutForm />
+      </section>
+    </div>
   );
 }
