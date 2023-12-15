8bdfd4d0fb1e2d7e720013c138cc391bff1fa0b2
mkizka <g.mkizka@gmail.com>
2023-12-16 00:49:16 +0900

    SigninPage

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,13 +1,13 @@
 import { redirect } from "next/navigation";
 
-import { getServerSession } from "@/_shared/utils/getServerSession";
+import { getSessionUserId } from "@/_shared/utils/getSessionUser";
 import { prisma } from "@/_shared/utils/prisma";
 
 import { AuthForm } from "./_components/AuthForm";
 
 export default async function SigninPage() {
-  const session = await getServerSession();
-  if (session) {
+  const userId = await getSessionUserId();
+  if (userId) {
     redirect("/");
   }
   const count = await prisma.credential.count();
