8f3a6b007ea554f561a75b2387b0608df9a025d9
mkizka <g.mkizka@gmail.com>
2023-12-23 15:21:05 +0900

    middleware追加

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const logLevel = (status: number) => {
+  if (status > 500) {
+    return "error";
+  }
+  if (status === 400) {
+    return "warn";
+  }
+  return "info";
+};
+
+export function middleware(request: NextRequest) {
+  const pathWithQuery = request.nextUrl.pathname + request.nextUrl.search;
+  const response = NextResponse.next();
+  // https://docs.railway.app/guides/logs#structured-logs
+  console[logLevel(response.status)](
+    JSON.stringify({
+      level: "info",
+      message: `${request.method} ${response.status} ${pathWithQuery}`,
+      url: request.nextUrl.pathname,
+      searchParams: Object.fromEntries(request.nextUrl.searchParams),
+      method: request.method,
+      headers: Object.fromEntries(request.headers),
+      date: new Date().toISOString(),
+      status: response.status,
+    }),
+  );
+  return response;
+}
