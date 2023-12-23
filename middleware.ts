7e0bc9c12304e467781b38c04a4d272e17f757c7
mkizka <g.mkizka@gmail.com>
2023-12-23 15:56:39 +0900

    responseオブジェクトは使えないので削除

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,19 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-const logLevel = (status: number) => {
-  if (status > 500) {
-    return "error";
-  }
-  if (status === 400) {
-    return "warn";
-  }
-  return "info";
-};
-
 export function middleware(request: NextRequest) {
   const pathWithQuery = request.nextUrl.pathname + request.nextUrl.search;
-  const response = NextResponse.next();
-  const level = logLevel(response.status);
   // https://docs.railway.app/guides/logs#structured-logs
-  console[level](
+  console.info(
     JSON.stringify({
-      level,
-      message: `${request.method} ${response.status} ${pathWithQuery}`,
+      level: "info",
+      message: `${request.method} ${pathWithQuery}`,
       url: request.nextUrl.pathname,
       searchParams: Object.fromEntries(request.nextUrl.searchParams),
       method: request.method,
       headers: Object.fromEntries(request.headers),
       date: new Date().toISOString(),
-      status: response.status,
     }),
   );
-  return response;
+  return NextResponse.next();
 }
