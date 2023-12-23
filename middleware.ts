479be8f810675c6b9cfd995ead84c6218a85efcd
mkizka <g.mkizka@gmail.com>
2023-12-23 18:43:36 +0900

    アクセスログでURL全部を表示

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,11 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const pathWithQuery = request.nextUrl.pathname + request.nextUrl.search;
   // https://docs.railway.app/guides/logs#structured-logs
   console.info(
     JSON.stringify({
       level: "info",
-      message: `${request.method} ${pathWithQuery}`,
+      message: `${request.method} ${request.url}`,
       url: request.nextUrl.pathname,
       searchParams: Object.fromEntries(request.nextUrl.searchParams),
       method: request.method,
