1fbeabdb62786c0bd6f70955fb394b9b2b52fc22
mkizka <g.mkizka@gmail.com>
2023-12-25 12:55:56 +0900

    /healthにversion追加

--- app/health/route.ts
+++ app/health/route.ts
@@ -1,6 +1,7 @@
 // Stryker disable all
 import { NextResponse } from "next/server";
 
+import pkg from "@/../package.json";
 import { env } from "@/_shared/utils/env";
 
 export const dynamic = "force-dynamic";
@@ -8,6 +9,7 @@ export const dynamic = "force-dynamic";
 export function GET() {
   return NextResponse.json({
     status: "ok",
+    version: pkg.version,
     env: {
       NODE_ENV: env.NODE_ENV,
       UNSOCIAL_HOST: env.UNSOCIAL_HOST,
