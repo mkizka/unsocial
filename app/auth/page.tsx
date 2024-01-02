f2538ef17412d6993af5b5d983d8d70d5397f48a
mkizka <g.mkizka@gmail.com>
2024-01-02 17:36:40 +0900

    リダイレクト判定の見直し

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,15 +1,20 @@
-import { redirect } from "next/navigation";
-
+import { systemUserService } from "@/_shared/service/systemUser";
 import { prisma } from "@/_shared/utils/prisma";
-import { getSessionUserId } from "@/_shared/utils/session";
+import { getSessionUser } from "@/_shared/utils/session";
 
 import { AuthForm } from "./_components/AuthForm";
 
+// 個人ユーザーの利用を想定しているため、システムユーザーでないアカウントが存在する場合は常にログイン画面を表示する
+const getFormType = async () => {
+  const systemUser = await systemUserService.findSystemUser();
+  const where = systemUser ? { NOT: { id: systemUser.id } } : {};
+  const count = await prisma.credential.count({ where });
+  return count > 0 ? "signIn" : "signUp";
+};
+
 export default async function SigninPage() {
-  const userId = await getSessionUserId();
-  if (userId) {
-    redirect("/");
-  }
-  const count = await prisma.credential.count();
-  return <AuthForm action={count > 0 ? "signIn" : "signUp"} />;
+  // 開発環境などでCookieが無効になることがあるため、
+  // セッションからユーザーIDが取れることだけでなくDBにユーザーが存在することも確認する
+  await getSessionUser({ redirect: true });
+  return <AuthForm action={await getFormType()} />;
 }
