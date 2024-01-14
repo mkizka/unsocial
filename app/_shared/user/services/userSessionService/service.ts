e02465490ccfbbe3a60947ca315b84f3f215eb96
mkizka <g.mkizka@gmail.com>
2024-01-14 16:16:17 +0900

    fix

--- app/_shared/user/services/userSessionService/service.ts
+++ app/_shared/user/services/userSessionService/service.ts
@@ -1,52 +1,11 @@
 import type { User } from "@prisma/client";
 import { redirect } from "next/navigation";
-import type { NextAuthOptions } from "next-auth";
 import NextAuth, { getServerSession } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 import { cache } from "react";
 
-import { userAuthService } from "@/_shared/user/services/userAuthService";
-import { env } from "@/_shared/utils/env";
-import { createLogger } from "@/_shared/utils/logger";
 import { prisma } from "@/_shared/utils/prisma";
 
-const logger = createLogger("next-auth");
-
-const authOptions: NextAuthOptions = {
-  secret: env.UNSOCIAL_SECRET,
-  callbacks: {
-    async session({ session, token }) {
-      session.user = {
-        id: token.id,
-      };
-      return session;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-  },
-  logger: {
-    error(code, message) {
-      logger.error(`${code} ${JSON.stringify(message)}`);
-    },
-    warn(code) {
-      // CredentialsProviderのみ使用する場合はNEXTAUTH_URLは不要なので、警告は無視する
-      if (code === "NEXTAUTH_URL") return;
-      logger.warn(code);
-    },
-  },
-  providers: [
-    CredentialsProvider({
-      // authorizeの型エラーを解消するために必要
-      // フォーム生成はしないので値は空にしておく
-      credentials: {},
-      authorize: userAuthService.authorize,
-    }),
-  ],
-};
+import { authOptions } from "./authOptions";
 
 export const handler = NextAuth(authOptions);
 
