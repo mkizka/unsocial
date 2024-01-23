b093921b999dc377b082f48561598711e6a34d2e
mkizka <g.mkizka@gmail.com>
2024-01-24 01:04:46 +0900

    fix

--- app/_shared/ui/Spinner/index.tsx
+++ app/_shared/ui/Spinner/index.tsx
@@ -19,7 +19,7 @@ export function Spinner({ className }: Props) {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
