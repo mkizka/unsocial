5986b0eb614cc72a1177e7a0dfe3aff3fad14243
mkizka <g.mkizka@gmail.com>
2024-01-25 00:46:15 +0900

    Revert "rm discrod"
    This reverts commit ce4f1cc5bdc77fdb0e33efebe624222d5d0eaf36.

---
+++ app/_shared/utils/postDiscord.ts
@@ -0,0 +1,35 @@
+import { env } from "./env";
+import { fetcher } from "./fetcher";
+import { createLogger } from "./logger";
+
+const logger = createLogger("postDiscord");
+
+const sliceText = (text: string, count: number) => {
+  const contents = [];
+  let temp = `${text}`;
+  while (temp.length > count) {
+    contents.push(temp.slice(0, count));
+    temp = temp.slice(count);
+  }
+  contents.push(temp);
+  return contents;
+};
+
+export const postDicord = async (text: string) => {
+  if (env.NODE_ENV === "test") {
+    return;
+  }
+  if (!env.UNSOCIAL_DISCORD_WEBHOOK_URL) {
+    logger.info("$DISCORD_WEBHOOK_URLがないのでDiscordへの投稿をスキップ");
+    return;
+  }
+  const contents = sliceText(text, 1990);
+  for (const content of contents) {
+    await fetcher(env.UNSOCIAL_DISCORD_WEBHOOK_URL, {
+      method: "POST",
+      body: JSON.stringify({ content: `\`\`\`\n${content}\n\`\`\`` }),
+    })
+      .then(() => logger.info(`Discordへの投稿に成功しました`))
+      .catch((e) => logger.warn(`Discordへの投稿に失敗しました: ${e}`));
+  }
+};
--- app/users/[userKey]/inbox/route.ts
+++ app/users/[userKey]/inbox/route.ts
@@ -2,6 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 import { createLogger } from "@/_shared/utils/logger";
+import { postDicord } from "@/_shared/utils/postDiscord";
 
 import { inboxService } from "./_services/inboxService";
 
@@ -11,6 +12,7 @@ export async function POST(request: NextRequest) {
   const error = await inboxService.perform(request);
   if (error) {
     logger[error.level](error.message);
+    await postDicord(error.messageFormatted);
     return NextResponse.json({}, { status: error.statusCode });
   }
   return NextResponse.json({});
