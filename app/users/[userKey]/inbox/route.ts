ce4f1cc5bdc77fdb0e33efebe624222d5d0eaf36
mkizka <g.mkizka@gmail.com>
2024-01-24 23:38:18 +0900

    rm discrod

--- app/users/[userKey]/inbox/route.ts
+++ app/users/[userKey]/inbox/route.ts
@@ -2,7 +2,6 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 import { createLogger } from "@/_shared/utils/logger";
-import { postDicord } from "@/_shared/utils/postDiscord";
 
 import { inboxService } from "./_services/inboxService";
 
@@ -12,7 +11,6 @@ export async function POST(request: NextRequest) {
   const error = await inboxService.perform(request);
   if (error) {
     logger[error.level](error.message);
-    await postDicord(error.messageFormatted);
     return NextResponse.json({}, { status: error.statusCode });
   }
   return NextResponse.json({});
