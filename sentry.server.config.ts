e64516fb5101ed4a391af45ab6c61139f97ed644
mkizka <g.mkizka@gmail.com>
2024-01-25 23:16:03 +0900

    Revert "Revert "CaptureConsole導入""
    This reverts commit a6578f24fde61e9b306295fa9b8d3c5f8255ab35.

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
+import { CaptureConsole } from "@sentry/integrations";
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
@@ -10,4 +10,5 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  integrations: [new CaptureConsole()],
 });
