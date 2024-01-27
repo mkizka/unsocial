2818ad3d6ab76ea9ac2ddea282944e64ff79d9c0
mkizka <g.mkizka@gmail.com>
2024-01-27 23:11:56 +0900

    fix

--- jest.config.ts
+++ jest.config.ts
@@ -12,7 +12,10 @@ const createJestConfig = nextJest({
 const customJestConfig: Config = {
   roots: ["<rootDir>/app"],
   testEnvironment: "@quramy/jest-prisma-node/environment",
-  setupFilesAfterEnv: ["<rootDir>/jest/setup.ts"],
+  setupFilesAfterEnv: [
+    "@quramy/prisma-fabbrica/scripts/jest-prisma",
+    "<rootDir>/jest/setup.ts",
+  ],
   globalSetup: "<rootDir>/jest/global-setup.ts",
   clearMocks: true,
   modulePathIgnorePatterns: ["<rootDir>/.next"],
