61cd6c69580c50ef7cd5571364539b7c1ae157b0
mkizka <g.mkizka@gmail.com>
2024-01-30 22:45:25 +0900

    Revert "fix"
    This reverts commit 2f12ed3dd8d6a913d0d415278e7d28e53d17df60.

--- app/users/[userKey]/inbox/_services/inboxService/handlers/create.ts
+++ app/users/[userKey]/inbox/_services/inboxService/handlers/create.ts
@@ -1,13 +1,23 @@
 import { apSchemaService } from "@/_shared/activitypub/apSchemaService";
 import { noteCreateService } from "@/_shared/note/services/noteCreateService";
+import { createLogger } from "@/_shared/utils/logger";
 
 import { ActivitySchemaValidationError } from "./errors";
 import { type InboxHandler } from "./shared";
+
+const logger = createLogger("inboxCreateService");
 
 export const handle: InboxHandler = async (activity) => {
   const parsedNote = apSchemaService.createSchema.safeParse(activity);
   if (!parsedNote.success) {
     return new ActivitySchemaValidationError(parsedNote.error, activity);
   }
-  await noteCreateService.create(parsedNote.data.object);
+  await noteCreateService.create(parsedNote.data.object).catch((error) => {
+    // https://www.prisma.io/docs/reference/api-reference/error-reference#p2002
+    if (error.code === "P2002") {
+      logger.info("すでに存在するノートを受信したのでスキップします");
+      return;
+    }
+    return error;
+  });
 };
