ae0f6d6ff85803f665e18c630d001295d1fab647
mkizka <g.mkizka@gmail.com>
2024-02-01 23:03:55 +0900

    テスト修正

--- app/_shared/factories/user.ts
+++ app/_shared/factories/user.ts
@@ -10,5 +10,6 @@ export const RemoteUserFactory = defineUserFactory({
   defaultData: async ({ seq }) => ({
     host: "remote.example.com",
     actorUrl: `https://remote.example.com/users/${seq}`,
+    inboxUrl: `https://remote.example.com/users/${seq}/inbox`,
   }),
 });
