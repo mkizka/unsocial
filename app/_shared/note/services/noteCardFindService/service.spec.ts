b5121228cec8692ccc45301aa194733887ecbda5
mkizka <g.mkizka@gmail.com>
2024-02-04 01:00:31 +0900

    テスト修正 途中まで

--- app/_shared/note/services/noteCardFindService/service.spec.ts
+++ app/_shared/note/services/noteCardFindService/service.spec.ts
@@ -1,7 +1,8 @@
-import type { Note, Prisma } from "@prisma/client";
-
-import { mockedPrisma } from "@/_shared/mocks/prisma";
+import { LikeFactory } from "@/_shared/factories/like";
+import { LocalNoteFactory, RemoteNoteFactory } from "@/_shared/factories/note";
+import { LocalUserFactory } from "@/_shared/factories/user";
 import { mockedGetSessionUserId } from "@/_shared/mocks/session";
+import { prisma } from "@/_shared/utils/prisma";
 
 import { noteCardFindService } from ".";
 
@@ -23,97 +24,112 @@ const dummyNote = {
 const dummySince = new Date("2023-01-01T00:00:00.000Z");
 const dummyUntil = new Date("2023-01-01T00:00:00.000Z");
 
-const includeNoteCard = {
-  user: true,
-  attachments: true,
-  likes: {
-    include: {
-      user: true,
-    },
-  },
-} satisfies Prisma.NoteInclude;
-
-const includeNoteCardWithReplies = {
-  ...includeNoteCard,
-  quote: {
-    include: includeNoteCard,
-  },
-  replyTo: {
-    include: includeNoteCard,
-  },
-  replies: {
-    include: {
-      ...includeNoteCard,
-      replies: true,
-    },
-  },
-} satisfies Prisma.NoteInclude;
-
 describe("noteCardFindService", () => {
   describe("findUniqueNoteCard", () => {
-    test("自分の投稿かついいね済み", async () => {
+    test("カード表示のための形式にしたノートを返す", async () => {
       // arrange
-      mockedPrisma.note.findUnique.mockResolvedValueOnce(
-        dummyNote as unknown as Note,
-      );
-      mockedGetSessionUserId.mockResolvedValueOnce("userId");
+      const data = await LocalNoteFactory.build();
+      const note = await prisma.note.create({ data, include: { user: true } });
       // act
-      const note = await noteCardFindService.findUniqueNoteCard("noteId");
+      const noteCard = await noteCardFindService.findUniqueNoteCard(note.id);
       // assert
-      expect(note).toMatchSnapshot();
-      expect(mockedPrisma.note.findUnique).toBeCalledWith({
-        where: { id: "noteId" },
-        include: includeNoteCardWithReplies,
+      expect(noteCard).toEqualPrisma({
+        attachmentUrls: [],
+        attachments: [],
+        content: note.content,
+        createdAt: expect.anyDate(),
+        id: note.id,
+        isLiked: false,
+        isMine: false,
+        likes: [],
+        publishedAt: expect.anyDate(),
+        quote: null,
+        quoteId: null,
+        quotedBy: null,
+        replies: [],
+        replyTo: null,
+        replyToId: null,
+        url: `/notes/${note.id}`,
+        user: {
+          actorUrl: null,
+          displayUsername: `@${note.user.preferredUsername}`,
+          email: null,
+          emailVerified: null,
+          host: note.user.host,
+          icon: null,
+          iconHash: null,
+          id: note.user.id,
+          image: null,
+          inboxUrl: null,
+          lastFetchedAt: null,
+          name: null,
+          preferredUsername: note.user.preferredUsername,
+          publicKey: null,
+          summary: null,
+          url: `/@${note.user.preferredUsername}`,
+        },
+        userId: note.userId,
       });
     });
-    test("自分の投稿でない", async () => {
+    test("自分の投稿かついいね済み", async () => {
       // arrange
-      mockedPrisma.note.findUnique.mockResolvedValueOnce(
-        dummyNote as unknown as Note,
+      const userId = await LocalUserFactory.createForConnect();
+      const noteId = await LocalNoteFactory.createForConnect({
+        user: {
+          connect: userId,
+        },
+      });
+      const like = await LikeFactory.create({
+        user: {
+          connect: userId,
+        },
+        note: {
+          connect: noteId,
+        },
+      });
+      mockedGetSessionUserId.mockResolvedValueOnce(like.userId);
+      // act
+      const noteCard = await noteCardFindService.findUniqueNoteCard(
+        like.noteId,
       );
+      // assert
+      expect(noteCard?.isLiked).toBe(true);
+      expect(noteCard?.isMine).toBe(true);
+    });
+    test("自分の投稿でなく未いいね", async () => {
+      // arrange
+      const like = await LikeFactory.create();
       mockedGetSessionUserId.mockResolvedValueOnce("otherUserId");
       // act
-      const note = await noteCardFindService.findUniqueNoteCard("noteId");
+      const noteCard = await noteCardFindService.findUniqueNoteCard(
+        like.noteId,
+      );
       // assert
-      expect(note?.isMine).toBe(false);
+      expect(noteCard?.isLiked).toBe(false);
+      expect(noteCard?.isMine).toBe(false);
     });
-    test("いいね済みでない", async () => {
+
+    test("他ホストのユーザーはURLを@user@example.comの形式にする", async () => {
       // arrange
-      mockedPrisma.note.findUnique.mockResolvedValueOnce({
-        ...dummyNote,
-        likes: [{ id: "likeId", userId: "otherUserId" }],
-      } as unknown as Note);
-      mockedGetSessionUserId.mockResolvedValueOnce("userId");
+      const note = await RemoteNoteFactory.create();
+      mockedGetSessionUserId.mockResolvedValueOnce(note.userId);
       // act
-      const note = await noteCardFindService.findUniqueNoteCard("noteId");
+      const noteCard = await noteCardFindService.findUniqueNoteCard(note.id);
       // assert
-      expect(note?.isLiked).toBe(false);
+      expect(noteCard!.user.url).toBe(
+        `/@${noteCard!.user.preferredUsername}@${noteCard!.user.host}`,
+      );
     });
-    test("自ホストのユーザーはドメインを省略", async () => {
-      // arrange
-      mockedPrisma.note.findUnique.mockResolvedValueOnce({
-        ...dummyNote,
-        user: {
-          preferredUsername: "preferredUsername",
-          host: "myhost.example.com",
-        },
-      } as unknown as Note);
-      mockedGetSessionUserId.mockResolvedValueOnce("userId");
-      // act
-      const note = await noteCardFindService.findUniqueNoteCard("noteId");
-      // assert
-      expect(note?.user.url).toBe("/@preferredUsername");
-    });
+
     test("ノートが無かった場合はgetSessionUserIdOrNullを呼ばない", async () => {
-      // arrange
-      mockedPrisma.note.findUnique.mockResolvedValueOnce(null);
       // act
-      const notes = await noteCardFindService.findUniqueNoteCard("noteId");
+      const noteCard = await noteCardFindService.findUniqueNoteCard("noteId");
       // assert
       expect(mockedGetSessionUserId).not.toHaveBeenCalled();
-      expect(notes).toEqual(null);
+      expect(noteCard).toEqual(null);
     });
   });
+  /*
   describe("findManyNoteCards", () => {
     test("正常系", async () => {
       // arrange
@@ -179,4 +195,5 @@ describe("noteCardFindService", () => {
       expect(notes).toEqual([]);
     });
   });
+  */
 });
