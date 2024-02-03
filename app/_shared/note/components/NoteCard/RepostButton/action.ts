90ba63ae29df84fed06ed3977329d8bad4cc9e65
mkizka <g.mkizka@gmail.com>
2024-02-04 01:06:51 +0900

    fix

---
+++ app/_shared/note/components/NoteCard/RepostButton/action.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function action(data: { noteId: string }) {
+  throw new Error("Not implemented");
+}
--- app/_shared/note/components/NoteCard/index.tsx
+++ app/_shared/note/components/NoteCard/index.tsx
@@ -2,13 +2,15 @@
 import { action as deleteAction } from "./DeleteButton/action";
 import { action as likeAction } from "./LikeButton/action";
 import { NoteCard, type NoteCardProps } from "./NoteCard";
+import { action as repostAction } from "./RepostButton/action";
 
-type Props = Omit<NoteCardProps, "onLike" | "onDelete">;
+type Props = Omit<NoteCardProps, "onRepost" | "onLike" | "onDelete">;
 
 export function NoteCardContainer(props: Props) {
   return (
     <NoteCard
       {...props}
+      onRepost={() => repostAction({ noteId: props.note.id })}
       onLike={() => likeAction({ noteId: props.note.id, content: "👍" })}
       onDelete={() => deleteAction(props.note.id)}
     />
