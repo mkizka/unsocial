a8d79bfa40e2c6debf0cd54c9c3d75684054bdf6
mkizka <g.mkizka@gmail.com>
2024-02-04 18:21:50 +0900

    リポストボタンでリポスト出来る

---
+++ app/_shared/note/components/NoteCard/RepostButton/action.spec.ts
@@ -0,0 +1,46 @@
+import { LocalNoteFactory } from "@/_shared/factories/note";
+import { LocalUserFactory } from "@/_shared/factories/user";
+import { mockedGetSessionUserId } from "@/_shared/mocks/session";
+import { prisma } from "@/_shared/utils/prisma";
+
+import { action } from "./action";
+
+describe("RepostButton/action", () => {
+  test("ノートをリポスト出来る", async () => {
+    // arrange
+    const noteToRepost = await LocalNoteFactory.create();
+    const user = await LocalUserFactory.create();
+    mockedGetSessionUserId.mockResolvedValue(user.id);
+    // act
+    await action({ noteId: noteToRepost.id });
+    // assert
+    const repostedNote = await prisma.note.findFirst({
+      where: {
+        userId: user.id,
+        quote: {
+          id: noteToRepost.id,
+        },
+      },
+    });
+    expect(repostedNote).not.toBeNull();
+  });
+  test("リポストしたノートを削除できる", async () => {
+    // arrange
+    const noteToRepost = await LocalNoteFactory.create();
+    const user = await LocalUserFactory.create();
+    mockedGetSessionUserId.mockResolvedValue(user.id);
+    await action({ noteId: noteToRepost.id });
+    // act
+    await action({ noteId: noteToRepost.id });
+    // assert
+    const repostedNote = await prisma.note.findFirst({
+      where: {
+        userId: user.id,
+        quote: {
+          id: noteToRepost.id,
+        },
+      },
+    });
+    expect(repostedNote).toBeNull();
+  });
+});
--- app/_shared/note/components/NoteCard/RepostButton/action.ts
+++ app/_shared/note/components/NoteCard/RepostButton/action.ts
@@ -1,5 +1,72 @@
 "use server";
 
-export async function action(data: { noteId: string }) {
-  throw new Error("Not implemented");
+import type { Prisma } from "@prisma/client";
+import assert from "assert";
+
+import { userSessionService } from "@/_shared/user/services/userSessionService";
+import { env } from "@/_shared/utils/env";
+import { prisma } from "@/_shared/utils/prisma";
+
+const repost = async (userId: string, noteId: string) => {
+  const note = await prisma.note.create({
+    data: {
+      userId,
+      quoteId: noteId,
+      content: "",
+      publishedAt: new Date(),
+    },
+    include: {
+      quote: {
+        include: {
+          user: true,
+        },
+      },
+    },
+  });
+  assert(note.quote, "ノートにquoteが存在しません");
+  if (note.quote.user.host !== env.UNSOCIAL_HOST) {
+    // TODO: Announceアクティビティを配送する
+  }
+};
+
+const include = {
+  quote: {
+    include: {
+      user: true,
+    },
+  },
+} satisfies Prisma.NoteInclude;
+
+type NoteWithQuote = Prisma.NoteGetPayload<{
+  include: typeof include;
+}>;
+
+const undoRepost = async (userId: string, noteWithQuote: NoteWithQuote) => {
+  await prisma.note.delete({
+    where: {
+      id: noteWithQuote.id,
+    },
+  });
+  assert(noteWithQuote.quote, "ノートにquoteが存在しません");
+  if (noteWithQuote.quote.user.host !== env.UNSOCIAL_HOST) {
+    // TODO: Deleteアクティビティを配送する
+  }
+};
+
+export async function action({ noteId }: { noteId: string }) {
+  const userId = await userSessionService.getUserId({ redirect: true });
+  const existingNoteWithQuote = await prisma.note.findFirst({
+    where: {
+      userId: userId,
+      quote: {
+        id: noteId,
+      },
+    },
+    include,
+  });
+  if (existingNoteWithQuote) {
+    await undoRepost(userId, existingNoteWithQuote);
+  } else {
+    await repost(userId, noteId);
+  }
 }
