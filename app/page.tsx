ded5edb47f828bfac8456bb7dd193df254a74f95
mkizka <g.mkizka@gmail.com>
2024-02-11 01:03:08 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { redirect } from "next/navigation";
 import { NoteForm } from "./_shared/note/components/NoteForm";
 import { Timeline } from "./_shared/note/components/Timeline";
 import { userSessionService } from "./_shared/user/services/userSessionService";
-import { env } from "./_shared/utils/env";
 import { prisma } from "./_shared/utils/prisma";
 
 export default async function Page() {
@@ -18,11 +17,7 @@ export default async function Page() {
   }
   const user = await prisma.user.findFirst({
     where: {
-      AND: [
-        // 連合用のシステムアカウントでない
-        { preferredUsername: { not: env.UNSOCIAL_HOST } },
-        { host: { equals: env.UNSOCIAL_HOST } },
-      ],
+      isAdmin: true,
     },
   });
   if (!user) {
