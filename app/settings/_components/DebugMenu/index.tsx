438ad99c27c63535a98883c6448605001f8b2d25
mkizka <g.mkizka@gmail.com>
2024-02-11 03:10:41 +0900

    debugmenu追加

---
+++ app/settings/_components/DebugMenu/index.tsx
@@ -0,0 +1,22 @@
+import { Card } from "@/_shared/ui/Card";
+import { env } from "@/_shared/utils/env";
+
+export function DebugMenu() {
+  const publicEnv =
+    env.NODE_ENV === "production"
+      ? {
+          NODE_ENV: env.NODE_ENV,
+          UNSOCIAL_HOST: env.UNSOCIAL_HOST,
+        }
+      : env;
+  return (
+    <Card>
+      <label className="block font-bold" htmlFor="name">
+        環境変数
+      </label>
+      <pre className="overflow-x-scroll">
+        {JSON.stringify(publicEnv, null, 2)}
+      </pre>
+    </Card>
+  );
+}
