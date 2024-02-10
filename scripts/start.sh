f9f1b227196eae14a5c6a75b79aed353a734a528
mkizka <g.mkizka@gmail.com>
2024-02-11 01:18:14 +0900

    pr test

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/bash
 set -eu
 
-PRISMA_VERSION=$(jq -r '.devDependencies.prisma' package.json)
-corepack enable pnpm
-for _ in {1..5}; do
-  pnpm -s dlx "prisma@$PRISMA_VERSION" db push --skip-generate && break
-  echo "Retrying..."
-  sleep 1
-done
-VERSION=$(jq -r '.version' package.json)
-SENTRY_RELEASE=$VERSION node server.js
+echo test
