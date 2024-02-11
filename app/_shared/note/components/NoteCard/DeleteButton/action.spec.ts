6667b2bfab108c9558bfd749a6b26147d428774d
mkizka <g.mkizka@gmail.com>
2024-02-11 16:58:23 +0900

    fix DeleteButton/action

--- app/_shared/note/components/NoteCard/DeleteButton/action.spec.ts
+++ app/_shared/note/components/NoteCard/DeleteButton/action.spec.ts
@@ -1,29 +1,44 @@
-import type { Note } from "@prisma/client";
-
 import { apReplayService } from "@/_shared/activitypub/apRelayService";
-import { mockedPrisma } from "@/_shared/mocks/prisma";
+import { LocalNoteFactory } from "@/_shared/factories/note";
+import { LocalUserFactory } from "@/_shared/factories/user";
 import { mockedGetSessionUserId } from "@/_shared/mocks/session";
+import { prisma } from "@/_shared/utils/prisma";
 
 import { action } from "./action";
 
 jest.mock("@/_shared/activitypub/apRelayService");
-const mockedRelayActivityToFollowers = jest.mocked(
-  apReplayService.relayActivityToFollowers,
-);
-
-const dummySessionUserId = "__id";
+const mockedRelay = jest.mocked(apReplayService.relay);
 
 describe("DeleteButton/action", () => {
-  test("正常系", async () => {
+  test("指定したノートを削除する", async () => {
     // arrange
-    mockedGetSessionUserId.mockResolvedValue(dummySessionUserId);
-    mockedPrisma.note.delete.mockResolvedValue({ id: "__noteId" } as Note);
+    const user = await LocalUserFactory.createForConnect();
+    mockedGetSessionUserId.mockResolvedValue(user.id);
+    const note = await LocalNoteFactory.create({
+      user: {
+        connect: user,
+      },
+    });
     // act
-    await action("__noteId");
+    await action(note.id);
     // assert
-    expect(mockedRelayActivityToFollowers).toHaveBeenCalledWith({
-      userId: dummySessionUserId,
+    expect(await prisma.note.findFirst()).toBeNull();
+    expect(mockedRelay).toHaveBeenCalledWith({
+      userId: user.id,
       activity: expect.objectContaining({ type: "Delete" }),
     });
+  });
+  test("指定したノートがログインユーザーのもので無ければエラーを返す", async () => {
+    // arrange
+    const user = await LocalUserFactory.create();
+    mockedGetSessionUserId.mockResolvedValue(user.id);
+    const anotherNote = await LocalNoteFactory.create();
+    // act
+    await expect(() => action(anotherNote.id)).rejects.toMatchObject({
+      code: "P2025",
+    });
+    // assert
+    expect(await prisma.note.findFirst()).not.toBeNull();
+    expect(mockedRelay).not.toHaveBeenCalled();
   });
 });
--- app/_shared/note/components/NoteCard/DeleteButton/action.ts
+++ app/_shared/note/components/NoteCard/DeleteButton/action.ts
@@ -7,9 +7,14 @@ import { prisma } from "@/_shared/utils/prisma";
 
 export async function action(noteId: string) {
   const userId = await userSessionService.getUserId({ redirect: true });
-  // TODO: 自分のじゃなかったらエラー吐く
-  await prisma.note.delete({ where: { id: noteId } });
-  await apReplayService.relayActivityToFollowers({
+  await prisma.note.delete({
+    where: {
+      id: noteId,
+      // ログインユーザーのノートで無ければ失敗する
+      userId,
+    },
+  });
+  await apReplayService.relay({
     userId,
     activity: activityStreams.delete({
       id: noteId,
