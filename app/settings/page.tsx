9f903ee064267a0c467377a3927409df20a3e883
mkizka <g.mkizka@gmail.com>
2024-02-17 15:08:53 +0900

    ヘッダー整理

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,31 +1,24 @@
-import { Card } from "@/_shared/ui/Card";
-
 import { DebugMenu } from "./_components/DebugMenu";
 import { IconSettingsForm } from "./_components/IconSettingsForm";
 import { ProfileFormContainer } from "./_components/ProfileFormContainer";
+import { SettingHeader } from "./_components/SettingHeader";
 import { SignOutForm } from "./_components/SignoutForm";
 
 export default function Page() {
   return (
     <div className="space-y-1">
       <section className="space-y-1">
-        <Card>
-          <h1 className="font-bold">プロフィール</h1>
-        </Card>
+        <SettingHeader>プロフィール</SettingHeader>
         <IconSettingsForm />
         <ProfileFormContainer />
       </section>
       <section className="space-y-1">
-        <Card>
-          <h1 className="font-bold">アカウント</h1>
-        </Card>
-        <SignOutForm />
+        <SettingHeader>サーバー情報</SettingHeader>
+        <DebugMenu />
       </section>
       <section className="space-y-1">
-        <Card>
-          <h1 className="font-bold">デバッグ</h1>
-        </Card>
-        <DebugMenu />
+        <SettingHeader>アカウント</SettingHeader>
+        <SignOutForm />
       </section>
     </div>
   );
