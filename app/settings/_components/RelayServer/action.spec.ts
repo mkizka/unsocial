f2ae0e0fb367993cd95d4d3735833fc62d5fe637
mkizka <g.mkizka@gmail.com>
2024-02-18 20:59:31 +0900

    テスト追加

---
+++ app/settings/_components/RelayServer/action.spec.ts
@@ -0,0 +1,43 @@
+import { http, HttpResponse } from "msw";
+
+import { server } from "@/_shared/mocks/server";
+import { systemUserService } from "@/_shared/user/services/systemUserService";
+
+import { action } from "./action";
+
+jest.mock("next/cache");
+
+describe("RelayServer/action", () => {
+  test("リレーサーバを登録しFollowアクティビティを配送できる", async () => {
+    // arrange
+    await systemUserService.findOrCreateSystemUser();
+    const formData = new FormData();
+    formData.append("inbox-url", "https://relay.example.com/inbox");
+    const inboxFn = jest.fn();
+    server.use(
+      http.post("https://relay.example.com/inbox", async ({ request }) => {
+        inboxFn(await request.json());
+        return HttpResponse.text("Accepted", { status: 202 });
+      }),
+    );
+    // act
+    const result = await action(null, formData);
+    // assert
+    expect(inboxFn).toHaveBeenCalledTimes(1);
+    expect(result).toMatchObject({
+      type: "success",
+      message: "リレーサーバーにFollowアクティビティを送信しました",
+    });
+  });
+  test("リレーサーバのURLが不正な場合はエラー", async () => {
+    // arrange
+    const formData = new FormData();
+    // act
+    const result = await action(null, formData);
+    // assert
+    expect(result).toMatchObject({
+      type: "error",
+      message: "入力内容が不正です",
+    });
+  });
+});
