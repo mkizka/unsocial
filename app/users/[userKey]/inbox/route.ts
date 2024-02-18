089f9c5eabbea61485a8f425403e7e9297a9f050
mkizka <g.mkizka@gmail.com>
2024-02-18 17:33:13 +0900

    inbox移動

--- app/users/[userKey]/inbox/route.ts
+++ app/users/[userKey]/inbox/route.ts
@@ -1,28 +1,3 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { POST } from "@/inbox/route";
 
-import { createLogger } from "@/_shared/utils/logger";
-
-import { inboxService } from "./_services/inboxService";
-
-const logger = createLogger("/users/[userId]/inbox");
-
-const getLevel = (statusCode: number) => {
-  if (statusCode >= 500) {
-    return "error";
-  }
-  return "warn";
-};
-
-export async function POST(request: NextRequest) {
-  const error = await inboxService.perform(request);
-  if (error) {
-    const level = getLevel(error.statusCode);
-    logger[level](error.message, {
-      headers: Object.fromEntries(request.headers),
-      body: await request.json(),
-    });
-    return NextResponse.json({}, { status: error.statusCode });
-  }
-  return NextResponse.json({});
-}
+export { POST };
