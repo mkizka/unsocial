5dcdd2c530ec6f5820bf37a2aed0ba8a9a8336fa
mkizka <g.mkizka@gmail.com>
2024-02-18 15:03:20 +0900

    testfix accept test

--- app/users/[userKey]/inbox/_services/inboxService/handlers/accept.spec.ts
+++ app/users/[userKey]/inbox/_services/inboxService/handlers/accept.spec.ts
@@ -1,44 +1,55 @@
-import { mockedPrisma } from "@/_shared/mocks/prisma";
+import { LocalToRemoteFollowFactory } from "@/_shared/factories/follow";
+import { RemoteUserFactory } from "@/_shared/factories/user";
+import { prisma } from "@/_shared/utils/prisma";
 
 import { handle } from "./accept";
 
-const dummyRemoteUser = {
-  id: "dummy_remote",
-} as never;
-
-const dummyLocalUser = {
-  id: "dummy_local",
-  host: "myhost.example.com",
-} as never;
-
 describe("inboxAcceptService", () => {
-  test("正常系", async () => {
+  test("リモートのフォロイーから送られたAcceptを元にフォロー状態を更新できる", async () => {
     // arrange
+    const follow = await prisma.follow.create({
+      data: await LocalToRemoteFollowFactory.build(),
+      include: {
+        followee: true,
+      },
+    });
     const activity = {
       type: "Accept",
       id: "https://remote.example.com/12345",
-      actor: "https://remote.example.com/u/dummy_remote",
+      actor: follow.followee.actorUrl,
       object: {
         type: "Follow",
-        actor: "https://myhost.example.com/users/dummy_local",
-        object: "https://remote.example.com/u/dummy_remote",
+        actor: `https://myhost.example.com/users/${follow.followerId}/activity`,
+        object: follow.followee.actorUrl,
       },
     };
-    mockedPrisma.user.findUnique.mockResolvedValue(dummyLocalUser);
     // act
-    const error = await handle(activity, dummyRemoteUser);
+    const error = await handle(activity, follow.followee);
     // assert
-    expect(mockedPrisma.follow.update).toHaveBeenCalledWith({
-      where: {
-        followeeId_followerId: {
-          followeeId: "dummy_remote",
-          followerId: "dummy_local",
-        },
-      },
-      data: {
-        status: "ACCEPTED",
-      },
-    });
     expect(error).toBeUndefined();
+    expect(await prisma.follow.findFirst()).toMatchObject({
+      status: "ACCEPTED",
+    });
+  });
+  test("対象のフォロワーが存在しない場合はエラーを返す", async () => {
+    // arrange
+    const remoteUser = await RemoteUserFactory.create();
+    const activity = {
+      type: "Accept",
+      id: "https://remote.example.com/12345",
+      actor: remoteUser.actorUrl,
+      object: {
+        type: "Follow",
+        actor: `https://myhost.example.com/users/not-found-id/activity`,
+        object: remoteUser.actorUrl,
+      },
+    };
+    // act
+    const error = await handle(activity, remoteUser);
+    // assert
+    expect(error).toBeInstanceOf(Error);
+    expect(error).toMatchObject({
+      message: "Acceptされたフォロワーが存在しませんでした",
+    });
   });
 });
