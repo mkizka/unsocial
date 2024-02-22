610cef8f16133f28b27719eb876918cf0af25a8e
mkizka <g.mkizka@gmail.com>
2024-02-23 02:14:35 +0900

    fix

--- prisma/migrations/20240218112717_fix_relay_server/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "RelayServerStatus" AS ENUM ('SENT', 'FAILED', 'ACCEPTED');
-
--- AlterTable
-ALTER TABLE "RelayServer" ADD COLUMN     "status" "RelayServerStatus" NOT NULL DEFAULT 'SENT';
--- prisma/migrations/20240222171419_add_relay/migration.sql
+++ prisma/migrations/20240222171419_add_relay/migration.sql
@@ -1,7 +1,11 @@
+-- CreateEnum
+CREATE TYPE "RelayServerStatus" AS ENUM ('SENT', 'FAILED', 'ACCEPTED');
+
 -- CreateTable
 CREATE TABLE "RelayServer" (
     "id" TEXT NOT NULL,
     "inboxUrl" TEXT NOT NULL,
+    "status" "RelayServerStatus" NOT NULL DEFAULT 'SENT',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "RelayServer_pkey" PRIMARY KEY ("id")
