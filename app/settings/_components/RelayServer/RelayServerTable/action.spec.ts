d9607e32f879cb93a47b4ea3b9f99798d9dd34b0
mkizka <g.mkizka@gmail.com>
2024-02-23 02:03:52 +0900

    send undo

--- app/settings/_components/RelayServer/RelayServerTable/action.spec.ts
+++ app/settings/_components/RelayServer/RelayServerTable/action.spec.ts
@@ -1,3 +1,6 @@
+import { http, HttpResponse } from "msw";
+
+import { server } from "@/_shared/mocks/server";
 import { prisma } from "@/_shared/utils/prisma";
 
 import { action } from "./action";
@@ -5,17 +8,30 @@ import { action } from "./action";
 jest.mock("next/cache");
 
 describe("RelayServerTable/action", () => {
-  test("指定したリレーサーバーを削除してrevalidatePathを呼ぶ", async () => {
+  test("指定したリレーサーバーを削除してUndoアクティビティを", async () => {
     // arrange
+    const inboxUrl = "https://relay.example.com/inbox";
     const relayServer = await prisma.relayServer.create({
       data: {
-        inboxUrl: "https://relay.example.com/inbox",
+        inboxUrl,
       },
     });
+    const inboxFn = jest.fn();
+    server.use(
+      http.post(inboxUrl, async ({ request }) => {
+        inboxFn(await request.json());
+        return HttpResponse.text("Accepted", { status: 202 });
+      }),
+    );
     // act
     await action(relayServer.id);
     // assert
     expect(await prisma.relayServer.findFirst()).toBeNull();
+    expect(inboxFn).toHaveBeenCalledWith(
+      expect.objectContaining({
+        type: "Undo",
+      }),
+    );
   });
   test("指定したリレーサーバーが存在しない場合は何もしない", async () => {
     // act
--- app/settings/_components/RelayServer/RelayServerTable/action.ts
+++ app/settings/_components/RelayServer/RelayServerTable/action.ts
@@ -2,6 +2,9 @@
 
 import { revalidatePath } from "next/cache";
 
+import { apRelayService } from "@/_shared/activitypub/apRelayService";
+import { systemUserService } from "@/_shared/user/services/systemUserService";
+import { activityStreams } from "@/_shared/utils/activitypub";
 import { prisma } from "@/_shared/utils/prisma";
 
 export const action = async (relayServerId: string) => {
@@ -17,6 +20,12 @@ export const action = async (relayServerId: string) => {
     where: {
       id: relayServerId,
     },
+  });
+  const systemUser = await systemUserService.findOrCreateSystemUser();
+  await apRelayService.relay({
+    userId: systemUser.id,
+    activity: activityStreams.undo(activityStreams.followPublic(systemUser.id)),
+    inboxUrl: existingRelayServer.inboxUrl,
   });
   revalidatePath("/settings");
 };
