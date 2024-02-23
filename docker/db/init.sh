376b3d55ae0685a305ef82d27feda4fdb747459f
mkizka <g.mkizka@gmail.com>
2024-02-23 16:27:08 +0900

    dbの初期化チェックを永続化

--- docker/db/init.sh
+++ docker/db/init.sh
@@ -4,4 +4,4 @@ createdb -U postgres mastodon
 createdb -U postgres misskey
 pg_restore -U postgres -d mastodon /docker-entrypoint-initdb.d/dumps/mastodon.dump
 pg_restore -U postgres -d misskey /docker-entrypoint-initdb.d/dumps/misskey.dump
-touch /tmp/db_initialized
+touch /var/lib/postgresql/data/db_initialized
