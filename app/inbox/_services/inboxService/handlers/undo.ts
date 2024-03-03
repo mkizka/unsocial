565e5e2275d8fe40d1e7aa2798696054cc25def4
mkizka <g.mkizka@gmail.com>
2024-03-03 23:41:23 +0900

    fix

--- app/inbox/_services/inboxService/handlers/undo.ts
+++ app/inbox/_services/inboxService/handlers/undo.ts
@@ -2,7 +2,9 @@ import type { User } from "@prisma/client";
 
 import { apSchemaService } from "@/_shared/activitypub/apSchemaService";
 import { userFindService } from "@/_shared/user/services/userFindService";
+import { createLogger } from "@/_shared/utils/logger";
 import { prisma } from "@/_shared/utils/prisma";
+import { isInstanceOfPrismaError } from "@/_shared/utils/prismaError";
 
 import type { InboxError } from "./errors";
 import {
@@ -17,6 +19,8 @@ type UndoInboxHandler = (
   actorUser: User,
 ) => Promise<InboxError | void>;
 
+const logger = createLogger("inboxUndoService");
+
 const undoFollow: UndoInboxHandler = async (activity, actorUser) => {
   const followee = await userFindService.findOrFetchUserByActor(
     activity.object.object,
@@ -26,14 +30,22 @@ const undoFollow: UndoInboxHandler = async (activity, actorUser) => {
       "アンフォローリクエストで指定されたフォロイーが存在しませんでした",
     );
   }
-  await prisma.follow.delete({
-    where: {
-      followeeId_followerId: {
-        followeeId: followee.id,
-        followerId: actorUser.id,
+  try {
+    await prisma.follow.delete({
+      where: {
+        followeeId_followerId: {
+          followeeId: followee.id,
+          followerId: actorUser.id,
+        },
       },
-    },
-  });
+    });
+  } catch (e) {
+    if (isInstanceOfPrismaError(e) && e.code === "P2025") {
+      logger.info("存在しないフォローに対してのUndoアクティビティでした");
+      return;
+    }
+    throw e;
+  }
 };
 
 const undoLike: UndoInboxHandler = async (activity, actorUser) => {
