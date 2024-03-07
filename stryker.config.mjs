3b63e1c1dcc5ca5db81f987a48f39810c6470046
mkizka <g.mkizka@gmail.com>
2024-03-07 12:51:18 +0900

    fix

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -8,7 +8,7 @@ const config = {
   incremental: true,
   incrementalFile: "reports/mutation/stryker-incremental.json",
   timeoutMS: 30000,
-  maxTestRunnerReuse: 15,
+  maxTestRunnerReuse: 20,
   ignoreStatic: true,
   cleanTempDir: "always",
   mutate: [
