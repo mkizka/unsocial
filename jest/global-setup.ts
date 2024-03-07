cef4724b152740b856116860118057c8cf3ca9ba
mkizka <g.mkizka@gmail.com>
2024-03-07 22:03:01 +0900

    ミューテーションテスト修正

--- jest/global-setup.ts
+++ jest/global-setup.ts
@@ -1,11 +1,8 @@
 import { execSync } from "child_process";
 
-const $ = (cmd: string) => execSync(cmd, { stdio: "inherit" });
-
 const globalSetup = () => {
-  $("docker compose -f jest/compose.yaml down -v");
-  $("docker compose -f jest/compose.yaml up -d --wait");
-  $("pnpm prisma migrate deploy");
+  execSync("docker compose -f jest/compose.yaml up -d --wait");
+  execSync("pnpm prisma migrate deploy");
 };
 
 export default globalSetup;
--- scripts/mutation-test.sh
+++ scripts/mutation-test.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 
-if [ -z "$BRANCH_NAME" ]; then
-  BRANCH_NAME=$(git branch --show-current)
-fi
-MUTATION_DIR=reports/mutation
-CACHE_PATH=$MUTATION_DIR/stryker-incremental.json
+# if [ -z "$BRANCH_NAME" ]; then
+#   BRANCH_NAME=$(git branch --show-current)
+# fi
+# MUTATION_DIR=reports/mutation
+# CACHE_PATH=$MUTATION_DIR/stryker-incremental.json
 
-rm -rf $MUTATION_DIR
-mkdir -p $MUTATION_DIR
+# rm -rf $MUTATION_DIR
+# mkdir -p $MUTATION_DIR
 
-echo "pr/${BRANCH_NAME}のキャッシュファイルをダウンロード..."
-curl -f --progress-bar -o $CACHE_PATH "https://gha.unsocial.dev/mutation-test/pr/$BRANCH_NAME/stryker-incremental.json"
-if [ ! -f $CACHE_PATH ]; then
-  echo "mainのキャッシュファイルをダウンロード..."
-  curl -f --progress-bar -o $CACHE_PATH https://gha.unsocial.dev/mutation-test/main/stryker-incremental.json
-fi
+# echo "pr/${BRANCH_NAME}のキャッシュファイルをダウンロード..."
+# curl -f --progress-bar -o $CACHE_PATH "https://gha.unsocial.dev/mutation-test/pr/$BRANCH_NAME/stryker-incremental.json"
+# if [ ! -f $CACHE_PATH ]; then
+#   echo "mainのキャッシュファイルをダウンロード..."
+#   curl -f --progress-bar -o $CACHE_PATH https://gha.unsocial.dev/mutation-test/main/stryker-incremental.json
+# fi
 
 pnpm stryker run "$@"
