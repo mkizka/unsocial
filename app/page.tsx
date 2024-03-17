1748eef221daaa0262c53255e0d916fbeb6b5157
Matsuda Kazuki <g.mkizka@gmail.com>
2024-03-17 19:32:12 +0900

    非ログイン時のリダイレクトやめる (#1564)

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,13 @@
-import { redirect } from "next/navigation";
-
 import { NoteForm } from "./_shared/note/components/NoteForm";
 import { Timeline } from "./_shared/note/components/Timeline";
 import { userSessionService } from "./_shared/user/services/userSessionService";
-import { prisma } from "./_shared/utils/prisma";
 
 export default async function Page() {
   const userId = await userSessionService.getUserId();
-  if (userId) {
-    return (
-      <>
-        <NoteForm />
-        <Timeline />
-      </>
-    );
-  }
-  const user = await prisma.user.findFirst({
-    where: {
-      isAdmin: true,
-    },
-  });
-  if (!user) {
-    redirect("/auth");
-  }
-  return redirect(`/@${user.preferredUsername}`);
+  return (
+    <>
+      {userId && <NoteForm />}
+      <Timeline />
+    </>
+  );
 }
